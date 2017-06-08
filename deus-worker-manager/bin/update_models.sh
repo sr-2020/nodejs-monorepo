81d16a698a545b946d7c7088504c434efe62b310
Alexander Bogdanov <andorn@gmail.com>
2017-06-08 12:54:08 +0300

    oops, should clone models from public url

--- deus-worker-manager/bin/update_models.sh
+++ deus-worker-manager/bin/update_models.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -d ./models ]; then
-    git clone git@github.com:sth-larp/deus-models.git models
+    git clone https://github.com/sth-larp/deus-models.git
 else
     cd ./models
     git fetch
