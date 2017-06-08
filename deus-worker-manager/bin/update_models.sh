154548df362ddc93f7beb483ff6f559068f87972
Alexander Bogdanov <andorn@gmail.com>
2017-06-08 12:58:12 +0300

    oops

--- deus-worker-manager/bin/update_models.sh
+++ deus-worker-manager/bin/update_models.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -d ./models ]; then
-    git clone https://github.com/sth-larp/deus-models.git
+    git clone https://github.com/sth-larp/deus-models.git models
 else
     cd ./models
     git fetch
