b8ad854ecacd58abc0ab1aab9f880ce528bc93e4
Andrei Matveiakin <a.matveyakin@gmail.com>
2017-06-08 22:42:30 +0200

    update _view

--- deus-models/some_model.js
+++ deus-models/some_model.js
@@ -1,23 +1,23 @@
-function getRussianSex(englishSex) {
-    switch (englishSex) {
+function getRussianSex(sex) {
+    switch (sex) {
         case "male":    return "мужской";
         case "female":  return "женский";
-        default:        return englishSex;
+        default:        return sex;
     }
 }
 
-function getHandicaps(data) {
-    if (data.length > 0) {
-        return data.join(", ");
+function getHandicaps(model) {
+    if (model.hp == 0) {
+        return "тяжелое ранение: только лежать";
     } else {
-        return "—";
+        return "нет";
     }
 }
 
 function getStartPage(model) {
     return {
         pageType: "list",
-        menuTitle: "Главное",
+        menuTitle: "Общая информация",
         body: {
             title: "Общая информация",
             items: [
@@ -39,7 +39,7 @@ function getStartPage(model) {
                 },
                 {
                     text: "Сумма на счёте",
-                    value: model.balance,
+                    value: 0,
                 },
                 {
                     text: "Корпорация",
@@ -52,21 +52,102 @@ function getStartPage(model) {
                 },
                 {
                     text: "Ограничения движения",
-                    value: getHandicaps(model.handicaps),
+                    value: getHandicaps(model),
                 },
             ],
         },
     };
 }
 
-function getMemoryPageItem(data) {
-    let textPieces = [];
-    if (data.text) textPieces.push(data.text);
-    if (data.url) textPieces.push(data.url);
+function getRussianConditionTag(tag) {
+    switch (tag) {
+        case "physiology":  return "Физиология";
+        case "mind":        return "Психология";
+        default:            return "";
+    }
+}
+
+function getConditionsPageItem(cond) {
     return {
-        text: data.title,
+        text: cond.text,
+        tag: getRussianConditionTag(cond.class),
+        icon: cond.class,
         details: {
-            header: data.title,
+            header: cond.text,
+            text: cond.details,
+        },
+    };
+}
+
+function getConditionsPage(model) {
+    return {
+        pageType: "list",
+        menuTitle: "Состояния",
+        body: {
+            title: "Ваши состояния",
+            items: model.conditions.map(getConditionsPageItem),
+        },
+    };
+}
+
+function getEnabledText(enabled) {
+    return enabled ? "ON" : "OFF";
+}
+
+function getEnabledColor(enabled) {
+    // TODO: Use magic color provided by the app
+    return enabled ? null : "#FF373F";
+}
+
+function getEnableActionText(enabled) {
+    return enabled ? "Выключить" : "Включить";
+}
+
+function isImplant(modifier) {
+    return modifier.class == "mechanical" ||
+           modifier.class == "biological";
+}
+
+function getImplantDetails(modifier) {
+    return modifier.details || ("Имплант " + modifier.displayName);
+}
+
+function getImplantsPageItem(modifier) {
+    return {
+        text: modifier.displayName,
+        value: getEnabledText(modifier.enabled),
+        valueColor: getEnabledColor(modifier.enabled),
+        details: {
+            header: modifier.displayName,
+            text: getImplantDetails(modifier),
+        },
+        action: {
+            text: getEnableActionText(modifier.enabled),
+            eventType: modifier.enabled ? "disableImplant" : "enableImplant",
+            data: modifier.mID,
+        },
+    };
+}
+
+function getImplantsPage(model) {
+    return {
+        pageType: "list",
+        menuTitle: "Импланты",
+        body: {
+            title: "Импланты",
+            items: model.modifiers.filter(isImplant).map(getImplantsPageItem),
+        },
+    };
+}
+
+function getMemoryPageItem(mem) {
+    let textPieces = [];
+    if (mem.text) textPieces.push(mem.text);
+    if (mem.url) textPieces.push(mem.url);
+    return {
+        text: mem.title,
+        details: {
+            header: mem.title,
             text: textPieces.join("\n"),
         },
     };
@@ -75,7 +156,7 @@ function getMemoryPageItem(data) {
 function getMemoryPage(model) {
     return {
         pageType: "list",
-        menuTitle: "Память",
+        menuTitle: "Воспоминания",
         body: {
             title: "Воспоминания",
             items: model.memory.map(getMemoryPageItem),
@@ -118,14 +199,24 @@ function getPages(model) {
         getStartPage(model),
         // TODO: Add insurance
         getMemoryPage(model),
-        getBodyPage(model),
-        // TODO: Add modifiers
+        getConditionsPage(model),
+        getImplantsPage(model),
+        //getBodyPage(model),
     ];
 }
 
 function getToolbar(model) {
     return {
         hitPoints: model.hp,
+    };
+}
+
+function getViewModel(model) {
+    return {
+        _id: model.characterId,
+        timestamp: model.timestamp,
+        toolbar: getToolbar(model),
+        pages: getPages(model),
     };
 }
 
@@ -155,11 +246,7 @@ module.exports = () => {
         },
 
         _view(model, viewModel) {
-            return {
-                _id: model.characterId,
-                toolbar: getToolbar(model),
-                pages: getPages(model),
-            };
+            viewModel = getViewModel(model);
         }
     };
 };
