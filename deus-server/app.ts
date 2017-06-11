91e34599a887979ad668a86d8c0340589e215182
Alexey Eremin <a.eremin.msu@gmail.com>
2017-06-11 23:25:09 +0200

    Refactor part of POST /events handler to separate class. Use async/await instead of promises in some places.

--- deus-server/app.ts
+++ deus-server/app.ts
@@ -2,17 +2,8 @@ import * as express from 'express'
 import * as http from 'http'
 import * as PouchDB from 'pouchdb';
 import bodyparser = require('body-parser')
-import * as Promise from 'promise'
+import { Connection, StatusAndBody } from "./connection";
 
-class StatusAndBody {
-  public status: number;
-  public body: any;
-}
-
-const race = (...promises) =>
-  new Promise((res, rej) => {
-    promises.forEach(p => p.then(v => res(v)).catch(err => rej(err)));
-  });
 
 class App {
   private app: express.Express = express();
@@ -48,36 +39,16 @@ class App {
 
     this.app.post('/events/:id', (req, res) => {
       const id: string = req.params.id;
-      this.viewmodelDb.get(id)
-        .then(() => {
-          const events = req.body.events;
-          if (!(events instanceof Array)) {
-            res.status(400).send("No events array in request");
-            return;
-          }
+      const events = req.body.events;
+      if (!(events instanceof Array)) {
+        res.status(400).send("No events array in request");
+        return;
+      }
 
-          const filterTimestamp = this.latestAcceptedEventTimestamp();
-          events.filter((value: any) => value.timestamp > filterTimestamp);
-
-          if (events.length == 0) {
-            res.status(202).send({
-              serverTime: this.currentTimestamp(),
-              id: id,
-            });
-          }
-          else {
-            const lastEventTimestamp = events[events.length - 1].timestamp;
-            let timeoutResponse = this.putEventsInDb(id, events)
-              .then(() => this.refreshModelTimeoutResponse(lastEventTimestamp));
-            let successRefreshResponse = this.refreshModelUpdatedResponse(id, lastEventTimestamp);
-            race(timeoutResponse, successRefreshResponse).then((statusAndBody: any) => {
-              statusAndBody.body.id = id;
-              statusAndBody.body.serverTime = this.currentTimestamp();
-              res.status(statusAndBody.status).send(statusAndBody.body)
-            })
-          }
-        })
-        .catch(err => res.status(404).send("Character with such id is not found"));
+      let connection = new Connection(this.eventsDb, this.viewmodelDb, this.timeout, this.latestExistingEventTimestamp());
+      connection.processEvents(id, events).then((s: StatusAndBody) => {
+        res.status(s.status).send(s.body);
+      });
     });
   }
 
@@ -89,33 +60,11 @@ class App {
     });
   }
 
-  putEventsInDb(id: string, events: Array<any>): Promise<any> {
-    return events.reduce((p, event) => {
-      return p.then(() => {
-        let eventWithCharId = event;
-        eventWithCharId.characterId = id;
-        return this.eventsDb.post(eventWithCharId);
-      });
-    }, Promise.resolve({}));
-  }
-
-  refreshModelUpdatedResponse(id: string, lastEventTimestamp: number): Promise<StatusAndBody> {
-    return new Promise((resolve, reject) => {
-      this.viewmodelDb.changes({ since: 'now', live: true, include_docs: true, doc_ids: [id] }).on('change', change => {
-        if (change.doc && change.doc.timestamp == lastEventTimestamp) {
-          delete change.doc._id;
-          delete change.doc._rev;
-          resolve({ status: 200, body: { viewModel: change.doc } });
-        }
-      });
-    });
-  }
-
   currentTimestamp(): number {
     return new Date().valueOf();
   }
 
-  latestAcceptedEventTimestamp(): number {
+  latestExistingEventTimestamp(): number {
     return 0;
   }
 
---
+++ deus-server/connection.ts
@@ -0,0 +1,74 @@
+export class StatusAndBody {
+  public status: number;
+  public body: any;
+}
+
+const race = (...promises) =>
+  new Promise((res, rej) => {
+    promises.forEach(p => p.then(v => res(v)).catch(err => rej(err)));
+  });
+
+
+
+export class Connection {
+  private latestSavedEventTimestamp = 0;
+
+  constructor(
+    private eventsDb: PouchDB.Database<{}>,
+    private viewmodelDb: PouchDB.Database<{ timestamp: number }>,
+    private timeout: number,
+    private latestExisingTimestamp: number) { }
+
+  async processEvents(id: string, events: any[]): Promise<any> /*Promise<StatusAndBody>*/ {
+    try {
+      await this.viewmodelDb.get(id);
+    } catch (e) {
+      if (e.status && e.status == 404 && e.reason && e.reason == "missing")
+        return {status: 404, body: "Character with such id is not found"};
+      else
+        throw e;
+    }
+
+    events = events.filter((value: any) => value.timestamp > this.latestExisingTimestamp);
+
+    try {
+      for (const event of events) {
+        let eventsWithCharId = event;
+        eventsWithCharId.characterId = id;
+        await this.eventsDb.post(eventsWithCharId);
+        this.latestSavedEventTimestamp = event.timestamp;
+      }
+    } catch (e) {
+      console.warn(e);
+      // TODO: Should we actually return error if only part of events were saved to DB?
+      return { status: 202, body: { id: id, serverTime: this.currentTimestamp(), timestamp: this.latestSavedEventTimestamp } };
+    }
+
+    return race(this.refreshModelUpdatedResponse(id, this.latestSavedEventTimestamp),
+                this.refreshModelTimeoutResponse(id));
+  }
+
+  private currentTimestamp(): number {
+    return new Date().valueOf();
+  }
+
+  refreshModelUpdatedResponse(id: string, lastEventTimestamp: number): Promise<StatusAndBody> {
+    return new Promise((resolve, reject) => {
+      this.viewmodelDb.changes({ since: 'now', live: true, include_docs: true, doc_ids: [id] }).on('change', change => {
+        if (change.doc && change.doc.timestamp == lastEventTimestamp) {
+          delete change.doc._id;
+          delete change.doc._rev;
+          resolve({ status: 200, body: { viewModel: change.doc, id: id, serverTime: this.currentTimestamp() } });
+        }
+      });
+    });
+  }
+
+  refreshModelTimeoutResponse(id: string): Promise<StatusAndBody> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ status: 202, body: { id: id, timestamp: this.latestSavedEventTimestamp, serverTime: this.currentTimestamp() } });
+      }, this.timeout);
+    });
+  }  
+}
