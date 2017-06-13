05f695916fe9bb147a482787cfbc80617cf56f2f
Alexey Eremin <a.eremin.msu@gmail.com>
2017-06-14 01:25:52 +0200

    Switch from callbacks to async/await

--- deus-server/app.spec.ts
+++ deus-server/app.spec.ts
@@ -1,5 +1,5 @@
 import * as http from 'http'
-import * as request from 'request'
+import * as rp from 'request-promise'
 import * as PouchDB from 'pouchdb';
 PouchDB.plugin(require('pouchdb-adapter-memory'));
 
@@ -33,93 +33,79 @@ describe('Express app', () => {
   })
 
   describe('/time', () => {
-    it('Returns approximately current time', done => {
-      request.get(address + '/time', (error, response, body) => {
-        expect(error).to.be.null;
-        expect(response.statusCode).to.eq(200);
-        expect(response.headers['content-type']).to.equal('application/json; charset=utf-8');
-        let parsedBody = JSON.parse(body);
-        expect(parsedBody.serverTime).to.be.approximately(new Date().valueOf(), 1000);
-        done();
-      });
+    it('Returns approximately current time', async () => {
+      const response = await rp.get(address + '/time',
+        { resolveWithFullResponse: true, json: { events: [] } }).promise();
+      expect(response.statusCode).to.eq(200);
+      expect(response.headers['content-type']).to.equal('application/json; charset=utf-8');
+      expect(response.body.serverTime).to.be.approximately(new Date().valueOf(), 1000);
     });
   });
 
   describe('/viewmodel', () => {
-    it('Returns viewmodel of existing character', done => {
-      request.get(address + '/viewmodel/existing_viewmodel', (error, response, body) => {
-        expect(error).to.be.null;
-        expect(response.statusCode).to.eq(200);
-        expect(response.headers['content-type']).to.equal('application/json; charset=utf-8');
-        let parsedBody = JSON.parse(body);
-        expect(parsedBody.serverTime).to.be.approximately(new Date().valueOf(), 1000);
-        expect(parsedBody.id).to.equal("existing_viewmodel");
-        expect(parsedBody.viewModel).to.deep.equal({ timestamp: 420, updatesCount: 0  });
-        done();
-      });
+
+    it('Returns viewmodel of existing character', async () => {
+      const response = await rp.get(address + '/viewmodel/existing_viewmodel',
+        { resolveWithFullResponse: true, json: {} }).promise();
+      expect(response.statusCode).to.eq(200);
+      expect(response.headers['content-type']).to.equal('application/json; charset=utf-8');
+      expect(response.body.serverTime).to.be.approximately(new Date().valueOf(), 1000);
+      expect(response.body.id).to.equal("existing_viewmodel");
+      expect(response.body.viewModel).to.deep.equal({ timestamp: 420, updatesCount: 0 });
     });
 
-    it('Returns 404 for non-existing character', done => {
-      request.get(address + '/viewmodel/5555', (error, response, body) => {
-        expect(response.statusCode).to.eq(404);
-        done();
-      });
+    it('Returns 404 for non-existing character', async () => {
+      const response = await rp.get(address + '/viewmodel/5555',
+        { resolveWithFullResponse: true, simple: false, json: {} }).promise();
+      expect(response.statusCode).to.eq(404);
     });
   });
 
   describe('POST /events', () => {
-    it('Returns 400 if no events field present', done => {
-      request.post(address + '/events/existing_viewmodel', { json: { foo: "bar" } }, (error, response, body) => {
-        expect(error).to.be.null;
-        expect(response.statusCode).to.eq(400);
-        done();
-      });
+    it('Returns 400 if no events field present', async () => {
+      const response = await rp.post(address + '/events/existing_viewmodel',
+        { resolveWithFullResponse: true, simple: false, json: { foo: "bar" } }).promise();
+      expect(response.statusCode).to.eq(400);
     });
 
-    it('Returns 400 if events field is not an array', done => {
-      request.post(address + '/events/existing_viewmodel', { json: { events: { foo: "bar" } } }, (error, response, body) => {
-        expect(error).to.be.null;
-        expect(response.statusCode).to.eq(400);
-        done();
-      });
+    it('Returns 400 if events field is not an array', async () => {
+      const response = await rp.post(address + '/events/existing_viewmodel',
+        { resolveWithFullResponse: true, simple: false, json: { events: { foo: "bar" } } }).promise();
+      expect(response.statusCode).to.eq(400);
     });
 
-    it('Returns 404 for non-existing character', done => {
-      request.post(address + '/events/5555', { json: { events: [] } }, (error, response, body) => {
-        expect(response.statusCode).to.eq(404);
-        done();
-      });
+    it('Returns 404 for non-existing character', async () => {
+      const response = await rp.post(address + '/events/5555',
+        { resolveWithFullResponse: true, simple: false, json: { events: [] } }).promise();
+      expect(response.statusCode).to.eq(404);
+    });
+    
+    it('Sets proper header', async () => {
+      const response = await rp.post(address + '/events/existing_viewmodel',
+        { resolveWithFullResponse: true, json: { events: [] } }).promise();
+      expect(response.statusCode).to.eq(202);
+      expect(response.headers['content-type']).to.equal('application/json; charset=utf-8');
     });
 
-    it('Sets proper header', done => {
-      request.post(address + '/events/existing_viewmodel', { json: { events: [] } }, (error, response, body) => {
-        expect(error).to.be.null;
-        expect(response.statusCode).to.eq(202);
-        expect(response.headers['content-type']).to.equal('application/json; charset=utf-8');
-        done();
-      });
-    });
-
-    it('Puts event into db', done => {
+    it('Puts event into db', async () => {
       const event = {
         eventType: "TestEvent",
         timestamp: 4365,
         data: { foo: "ambar" }
       };
-      request.post(address + '/events/existing_viewmodel', { json: { events: [event] } }, (error, response, body) => {
-        expect(error).to.be.null;
-        expect(response.statusCode).to.eq(202);
-        eventsDb.allDocs({ include_docs: true }).then(result => {
-          expect(result.rows.length).to.equal(1);
-          const doc: any = result.rows[0].doc;
-          expect(doc).to.deep.include(event);
-          expect(doc).to.deep.include({ characterId: "existing_viewmodel" });
-          done();
-        })
-      });
+      const response = await rp.post(address + '/events/existing_viewmodel',
+        { resolveWithFullResponse: true, json: { events: [event] } }).promise();
+
+      expect(response.statusCode).to.eq(202);
+      return eventsDb.allDocs({ include_docs: true }).then(result => {
+        expect(result.rows.length).to.equal(1);
+        const doc: any = result.rows[0].doc;
+        expect(doc).to.deep.include(event);
+        expect(doc).to.deep.include({ characterId: "existing_viewmodel" });
+      })
     });
 
-    it('Returns viewmodel in case if processed in time', done => {
+    it('Returns viewmodel in case if processed in time', async () => {
       const event = {
         eventType: "TestEvent",
         timestamp: 4365
@@ -129,7 +115,7 @@ describe('Express app', () => {
           const changeDoc = change.doc;
           viewModelDb.get('existing_viewmodel').then(doc => {
             viewModelDb.put({
-              _id: 'existing_viewmodel', 
+              _id: 'existing_viewmodel',
               _rev: doc._rev,
               timestamp: changeDoc.timestamp,
               updatesCount: doc.updatesCount + 1
@@ -138,51 +124,48 @@ describe('Express app', () => {
         }
       });
 
-      request.post(address + '/events/existing_viewmodel', { json: { events: [event] } }, (error, response, body) => {
-        expect(error).to.be.null;
-        expect(response.statusCode).to.eq(200);
-        expect(body.serverTime).to.be.approximately(new Date().valueOf(), 1000);
-        expect(body.id).to.equal("existing_viewmodel");
-        expect(body.viewModel).to.deep.equal({ timestamp: 4365, updatesCount: 1 });
-        done();
-      });
+      const response = await rp.post(address + '/events/existing_viewmodel',
+        { resolveWithFullResponse: true, json: { events: [event] } }).promise();
+      expect(response.statusCode).to.eq(200);
+      expect(response.body.serverTime).to.be.approximately(new Date().valueOf(), 1000);
+      expect(response.body.id).to.equal("existing_viewmodel");
+      expect(response.body.viewModel).to.deep.equal({ timestamp: 4365, updatesCount: 1 });
     });
 
-    it('Returns Accepted in case if not processed in time', done => {
+    it('Returns Accepted in case if not processed in time', async () => {
       const event = {
         eventType: "TestEvent",
         timestamp: 4365
       };
 
-      request.post(address + '/events/existing_viewmodel', { json: { events: [event] } }, (error, response, body) => {
-        expect(error).to.be.null;
-        expect(response.statusCode).to.eq(202);
-        expect(response.headers['content-type']).to.equal('application/json; charset=utf-8');
-        expect(body.serverTime).to.be.approximately(new Date().valueOf(), 1000);
-        expect(body.id).to.equal("existing_viewmodel");
-        expect(body.timestamp).to.equal(4365);
-        done();
-      });
-    });    
-/*
-    it('Deduplicates events with same timestamp', done => {
-      const event = {
-        eventType: "TestEvent",
-        timestamp: 4365,
-        data: { foo: "ambar" }
-      };
-      request.post(address + '/events/existing_viewmodel', { json: { events: [event] } }, (error, response, body) => {
-        expect(error).to.be.null;
-        expect(response.statusCode).to.eq(202);
-        request.post(address + '/events/existing_viewmodel', { json: { events: [event] } }, (error, response, body) => {
-          eventsDb.allDocs({ include_docs: true }).then(result => {
-            expect(result.rows.length).to.equal(1);
-            done();
-          })
-        });
-      });
+      const response = await rp.post(address + '/events/existing_viewmodel',
+        { resolveWithFullResponse: true, json: { events: [event] } }).promise();
+
+      expect(response.statusCode).to.eq(202);
+      expect(response.headers['content-type']).to.equal('application/json; charset=utf-8');
+      expect(response.body.serverTime).to.be.approximately(new Date().valueOf(), 1000);
+      expect(response.body.id).to.equal("existing_viewmodel");
+      expect(response.body.timestamp).to.equal(4365);
     });
-*/
+
+    /*
+        it('Deduplicates events with same timestamp', done => {
+          const event = {
+            eventType: "TestEvent",
+            timestamp: 4365,
+            data: { foo: "ambar" }
+          };
+          request.post(address + '/events/existing_viewmodel', { json: { events: [event] } }, (error, response, body) => {
+            expect(error).to.be.null;
+            expect(response.statusCode).to.eq(202);
+            request.post(address + '/events/existing_viewmodel', { json: { events: [event] } }, (error, response, body) => {
+              eventsDb.allDocs({ include_docs: true }).then(result => {
+                expect(result.rows.length).to.equal(1);
+                done();
+              })
+            });
+          });
+        });
+    */
   });
 });
-
