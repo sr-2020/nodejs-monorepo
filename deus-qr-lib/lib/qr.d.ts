327e1f3af4ea402fa0c5c3b3d3d249b74dd767ce
Alexey Eremin <a.eremin.msu@gmail.com>
2017-06-21 00:32:40 +0200

    Build (?) as library - 2

---
+++ deus-qr-lib/lib/qr.d.ts
@@ -0,0 +1,18 @@
+export declare class QrData {
+    type: number;
+    kind: number;
+    validUntil: number;
+    payload: string;
+}
+export declare class FormatError implements Error {
+    message: string;
+    name: string;
+    constructor(message: string);
+}
+export declare class ValidationError implements Error {
+    message: string;
+    name: string;
+    constructor(message: string);
+}
+export declare function decode(content: string): QrData;
+export declare function encode(data: QrData): string;
