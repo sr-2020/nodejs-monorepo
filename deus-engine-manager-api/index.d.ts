eb146b465bf1a050b92030b86636235949964660
Alexander Bogdanov <andorn@gmail.com>
2017-07-17 19:16:04 +0300

    init

---
+++ deus-engine-manager-api/index.d.ts
@@ -0,0 +1,59 @@
+export type LogLevel = 'debug' | 'info' | 'warn' | 'error';
+export type LogSource = 'default' | 'manager' | 'engine' | 'model';
+
+export type Event = {
+    characterId: string,
+    eventType: string,
+    timestamp: number,
+    data?: any
+}
+
+export type SyncEvent = Event & { eventType: '_RefreshModel' }
+
+export type EngineContext = {
+    timestamp: number,
+    [key: string]: any
+}
+
+export type EngineMessageEvents = {
+    type: 'events'
+    context: any
+    events: Event[]
+}
+
+export type EngineMessageConfigure = {
+    type: 'configure'
+    data: any
+}
+
+export type EngineMessage = EngineMessageEvents | EngineMessageConfigure;
+
+export type EngineResultOk = {
+    status: 'ok'
+    baseModel: any,
+    workingModel: any,
+    viewModels: { [base: string]: any }
+}
+
+export type EngineResultError = {
+    status: 'error',
+    error: any
+}
+
+export type EngineResult = EngineResultOk | EngineResultError;
+
+export type EngineReplyResult = EngineResult & { type: 'result' };
+
+export interface EngineReplyLog {
+    type: 'log',
+    source: string,
+    level: LogLevel,
+    msg: string,
+    params: any[]
+}
+
+export interface EngineReplyReady {
+    type: 'ready'
+}
+
+export type EngineReply = EngineReplyReady | EngineReplyResult | EngineReplyLog;
