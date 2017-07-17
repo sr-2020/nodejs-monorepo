a201105a610189c2d0f8cac1d8940ea59f99fd80
Alexander Bogdanov <andorn@gmail.com>
2017-07-17 20:55:03 +0300

    rewrite Worker::process with Rx

--- deus-worker-manager/src/worker.ts
+++ deus-worker-manager/src/worker.ts
@@ -3,10 +3,9 @@ import * as ChildProcess from 'child_process';
 import * as Rx from 'rxjs/Rx';
 import { fromStream } from './utils';
 
-import { Event, EngineMessage, EngineReply, EngineResult } from 'deus-engine-manager-api';
+import { Event, EngineMessage, EngineReply, EngineResult, EngineResultOk, EngineResultError } from 'deus-engine-manager-api';
 
 import { LoggerInterface, LogLevel } from './logger';
-// import { Event } from './events_source';
 import { Catalogs } from './catalogs_storage';
 
 export class Worker extends EventEmitter {
@@ -16,8 +15,9 @@ export class Worker extends EventEmitter {
         message: Rx.Observable<EngineReply>,
         error: Rx.Observable<any>,
         exit: Rx.Observable<any>,
+        stop: Rx.Observable<any>,
         data: Rx.Observable<string>
-    } | null;
+    };
 
     lastOutput: string[] = [];
     startedAt: number;
@@ -43,14 +43,14 @@ export class Worker extends EventEmitter {
             let data = out.merge(err).takeUntil(stop);
 
             this.rx = {
-                message, exit, error, data
+                message, exit, error, stop, data
             };
 
             // subscribe for logs early
             this.rx.message.filter((msg) => msg.type == 'log').subscribe(this.handleLogMessage);
             this.rx.data.subscribe(this.handleOutput);
 
-            let ready = this.rx.message.filter((msg) => msg.type == 'ready').first();
+            let ready = this.rx.message.filter((msg) => msg.type == 'ready').take(1);
 
             this.rx.exit.takeUntil(ready).subscribe(() => reject(new Error("Could't start child process")));
             this.rx.error.takeUntil(ready).subscribe(() => reject(new Error("Could't start child process")));
@@ -72,7 +72,7 @@ export class Worker extends EventEmitter {
             this.child.removeAllListeners();
             this.child.kill();
             this.child = null;
-            this.rx = null;
+            this.rx = null as any;
         }
         return this;
     }
@@ -119,34 +119,13 @@ export class Worker extends EventEmitter {
 
     async process(syncEvent: Event, model: any, events: Event[]): Promise<EngineResult> {
         this.lastOutput = [];
+
         return new Promise<EngineResult>((resolve, reject) => {
             if (!this.child) return reject(new Error('No child process'));
 
-            const onResult = (message: any) => {
-                if (message.type == 'result') {
-                    if (this.child) {
-                        this.child.removeListener('message', onResult);
-                        this.child.removeListener('error', onError);
-                    }
-
-                    let { status, baseModel, workingModel, viewModels } = message;
-
-                    resolve({ status, baseModel, workingModel, viewModels });
-                }
-            };
-
-            const onError = (err: any) => {
-                if (this.child) {
-                    this.child.removeListener('message', onResult);
-                    this.child.removeListener('error', onError);
-                    this.child.removeListener('exit', onError);
-                }
-                reject('Worker error');
-            };
-
-            this.child.on('message', onResult);
-            this.child.on('error', onError);
-            this.child.on('exit', onError);
+            let result = this.rx.message.filter((m) => m.type == 'result') as Rx.Observable<EngineResult>;
+            result.first().subscribe(resolve);
+            this.rx.stop.takeUntil(result).take(1).subscribe(() => reject('Worker error'));
 
             this.send({ type: 'events', context: model, events });
 
