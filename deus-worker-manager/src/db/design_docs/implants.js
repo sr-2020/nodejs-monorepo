39e7f8ec9cc6a8872ee0bea1a8587ab6e36df30a
Alexander Bogdanov <andorn@gmail.com>
2017-08-04 19:10:21 +0300

    view: implants by id

--- deus-worker-manager/src/db/design_docs/implants.js
+++ deus-worker-manager/src/db/design_docs/implants.js
@@ -1,7 +1,7 @@
 module.exports = {
     dbs: ['models'],
 
-    version: 0,
+    version: 1,
 
     views: {
         'by-class': {
@@ -11,6 +11,28 @@ module.exports = {
                         var mod = doc.modifiers[i];
                         if (mod.class != '_internal') {
                             emit(mod.class, {
+                                _id: doc._id,
+                                class: mod.class,
+                                system: mod.system,
+                                implantId: mod.id,
+                                displayName: mod.displayName,
+                                enabled: mod.enabled
+                            });
+                        }
+                    }
+                }
+            },
+
+            reduce: '_count'
+        },
+
+        'by-implant-id': {
+            map: function(doc) {
+                if (doc.modifiers) {
+                    for (var i = 0; i < doc.modifiers.length; i++) {
+                        var mod = doc.modifiers[i];
+                        if (mod.class != '_internal') {
+                            emit(mod.id, {
                                 _id: doc._id,
                                 class: mod.class,
                                 system: mod.system,
