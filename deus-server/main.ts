c864ec16e8d6a501c659cc38d79691e9c4741009
Alexey Eremin <a.eremin.msu@gmail.com>
2018-03-30 16:31:40 +0200

    Wait for CouchDB databases to become available before starting to better support in-Docker usage

--- deus-server/main.ts
+++ deus-server/main.ts
@@ -1,6 +1,7 @@
 import * as PouchDB from 'pouchdb';
 import { TypedJSON } from 'typedjson/js/typed-json';
 import { TSMap } from 'typescript-map';
+import * as waitOn from 'wait-on';
 import * as winston from 'winston';
 import App from './app';
 import { Configuration } from './settings';
@@ -21,17 +22,29 @@ try {
   const databasesConfig = config.databases;
   const authOptions = { auth: { username: databasesConfig.username, password: databasesConfig.password } };
 
-  const viewmodelDbs = new TSMap<string, PouchDB.Database<{ timestamp: number }>>(
-    databasesConfig.viewModels.map((v) => [v.type, new PouchDB(v.url, authOptions)]));
+  const requiredDbUrls = databasesConfig.viewModels.map((v) => v.url);
+  requiredDbUrls.push(databasesConfig.events, databasesConfig.accounts);
 
-  new App(logger,
-    new PouchDB(databasesConfig.events, authOptions),
-    viewmodelDbs,
-    new PouchDB(databasesConfig.accounts, authOptions),
-    config.settings).listen(config.port);
+  const opts = {
+    resources: requiredDbUrls,
+  };
 
-  logger.info('Ready to accept requests.');
+  waitOn(opts, (err) => {
+    if (err)
+      throw err;
+    else {
+      const viewmodelDbs = new TSMap<string, PouchDB.Database<{ timestamp: number }>>(
+        databasesConfig.viewModels.map((v) => [v.type, new PouchDB(v.url, authOptions)]));
 
+      new App(logger,
+        new PouchDB(databasesConfig.events, authOptions),
+        viewmodelDbs,
+        new PouchDB(databasesConfig.accounts, authOptions),
+        config.settings).listen(config.port);
+
+      logger.info('Ready to accept requests.');
+    }
+  });
 } catch (e) {
   logger.error('Error during server startup: ' + e);
 }
