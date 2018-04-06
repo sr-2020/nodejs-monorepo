1df9be0051fb8584bf3df6b513e6773277d00be4
Alexey Eremin <a.eremin.msu@gmail.com>
2018-04-07 01:52:03 +0200

    Add some logging

--- deus-model-engine/src/utils.ts
+++ deus-model-engine/src/utils.ts
@@ -1,6 +1,7 @@
 import glob = require('glob');
 import { merge as _merge } from 'lodash'
 import * as Path from 'path';
+import Logger from './logger';
 
 export function requireDir(dir: string, merge = _merge): any {
     const extensions = Object.keys(require.extensions).join('|');
@@ -8,10 +9,11 @@ export function requireDir(dir: string, merge = _merge): any {
 
     return files.reduce((m, f) => {
         let src;
-
         try {
+            Logger.info('engine', `Loading model from file ${f}`);
             src = require(f);
         } catch (e) {
+            Logger.warn('engine', `Haven't managed to load models from ${f}, trying file ${Path.join(process.cwd(), f)}`);
             src = require(Path.join(process.cwd(), f));
         }
 
