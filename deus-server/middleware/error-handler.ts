f56f763578adfd80a1d1d49c2e2c77b2719c1929
Alexey Eremin <a.eremin.msu@gmail.com>
2018-04-23 23:25:21 +0200

    Return proper error logging

--- deus-server/middleware/error-handler.ts
+++ deus-server/middleware/error-handler.ts
@@ -1,10 +1,14 @@
 import {Middleware, ExpressErrorMiddlewareInterface, ExpressMiddlewareInterface} from "routing-controllers";
+import { LoggerToken } from "../services/logger";
+import { Container } from "typedi";
+import { createLogData } from "../utils";
 
 @Middleware({ type: "after" })
 export class LoggingErrorHandler implements ExpressErrorMiddlewareInterface {
     error(error: any, request: any, response: any, next: (err: any) => any) {
-        // TOOD: Proper logging
-        // console.error("OH NO, ERROR:", error.message);
+        const logData = createLogData(request, error.httpCode || 500);
+        logData.msg = error.message || '';
+        Container.get(LoggerToken).error('Returning error response', logData)
         next(error);
     }
 }
