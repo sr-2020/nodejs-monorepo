799d4c1331dc4a8db021b43292f408ceb0275aaf
Alexey Eremin <a.eremin.msu@gmail.com>
2018-05-20 12:18:32 +0200

    Extract EventsProcessor class for further re-use in other controllers.

--- deus-server/controllers/events.controller.ts
+++ deus-server/controllers/events.controller.ts
@@ -14,12 +14,7 @@ import { LoggerToken } from "../services/logger";
 import { ApplicationSettingsToken } from "../services/settings";
 import { Connection, StatusAndBody } from "../connection";
 import { sendGenericPushNotification, makeVisibleNotificationPayload } from "../push-helpers";
-
-interface Event {
-  eventType: string;
-  timestamp: number;
-  data?: any;
-}
+import { Event, EventsProcessor } from "../events.processor";
 
 export interface EventsRequest {
   events: Event[]
@@ -46,7 +41,7 @@ export class EventsController {
       const response = {
         serverTime: currentTimestamp(),
         id,
-        timestamp: await this.cutTimestamp(id),
+        timestamp: await new EventsProcessor().cutTimestamp(id),
       };
       return response;
     } catch (e) {
@@ -56,8 +51,7 @@ export class EventsController {
 
   @Post("/events/:id")
   async post( @CurrentUser() user: Account, @Param("id") id: string, @Body() body: EventsRequest,
-    @Req() req: express.Request, @Res() res: express.Response)
-  {
+    @Req() req: express.Request, @Res() res: express.Response) {
     try {
       id = await canonicalId(id);
       await checkAccess(user, id);
@@ -66,113 +60,23 @@ export class EventsController {
       if (!(events instanceof Array))
         throw new BadRequestError('No events array in request');
 
-      events = await this.processTokenUpdateEvents(id, events);
-      const eventsForLogBefore = CleanEventsForLogging(events);
-      const isMobileClient = events.some((event) => event.eventType == '_RefreshModel');
-      if (isMobileClient) {
-        events = this.filterRefreshModelEventsByTimestamp(id, events);
-      }
-      const cutTimestamp = await this.cutTimestamp(id);
-      if (!isMobileClient && events.some((event) => event.timestamp <= cutTimestamp))
-        throw new HttpError(409, "Can't accept event with timestamp earlier than cut timestamp: they won't get processed!");
-      events = events.filter((value: any) => value.timestamp > cutTimestamp);
-      if (isMobileClient) {
-        if (this.dbContainer().connections.has(id))
-          throw new HttpError(429, 'Multiple connections from one client are not allowed');
+      const processor = new EventsProcessor();
+      const viewModelTimestampBefore = await processor.latestExistingMobileEventTimestamp(id);
+      const s = await processor.process(id, events);
 
-        this.dbContainer().connections.set(id, new Connection(this.dbContainer().eventsDb(),
-          Container.get(ApplicationSettingsToken).viewmodelUpdateTimeout));
-
-        const eventsForLogAfter = CleanEventsForLogging(events);
-        const viewModelTimestampBefore = await this.latestExistingMobileEventTimestamp(id);
-        const s = await this.dbContainer().connections.get(id).processEvents(id, events);
-        if (s.status == 200) {
-          await this.sendPushNotifications(viewModelTimestampBefore, s.body.viewModel);
-          this.logSuccessfulResponse(req, eventsForLogBefore, eventsForLogAfter, s.status);
-        }
-        else {
-          this.logHalfSuccessfulResponse(req, eventsForLogBefore, eventsForLogAfter, s.status);
-        }
-        this.dbContainer().connections.delete(id);
-        res.status(s.status);
-        return s.body;
-      } else {
-        // In this case we don't need to subscribe for viewmodel updates or
-        // block other clients from connecting.
-        // So we don't add Connection to this.connections.
-        const connection = new Connection(this.dbContainer().eventsDb(), 0);
-        const s = await connection.processEvents(id, events);
-        res.status(s.status);
-        return s.body;
+      if (s.status == 200) {
+        await this.sendPushNotifications(viewModelTimestampBefore, s.body.viewModel);
+        this.logSuccessfulResponse(req, CleanEventsForLogging(events), s.status);
       }
+      else {
+        this.logHalfSuccessfulResponse(req, CleanEventsForLogging(events), s.status);
+      }
+      res.status(s.status);
+      return s.body;
     } catch (e) {
       returnCharacterNotFoundOrRethrow(e);
     }
 
-  }
-
-  private dbContainer() {
-    return Container.get(DatabasesContainerToken);
-  }
-
-  private async cutTimestamp(id: string): Promise<number> {
-    const [currentViewmodelTimestamp, lastEventTimeStamp] = await Promise.all([
-      this.latestViewmodelTimestamp(id),
-      this.latestExistingMobileEventTimestamp(id)
-    ]);
-    return Math.max(currentViewmodelTimestamp, lastEventTimeStamp);
-  }
-
-  private async latestViewmodelTimestamp(id: string): Promise<number> {
-    return (await this.dbContainer().viewModelDb('mobile').get(id)).timestamp;
-  }
-
-  private async latestExistingMobileEventTimestamp(id: string): Promise<number> {
-    const docs = await this.dbContainer().eventsDb().query<any>(characterIdTimestampOnlyRefreshesView,
-      { include_docs: true, descending: true, endkey: [id], startkey: [id, {}], limit: 1 });
-    return docs.rows.length ? docs.rows[0].doc.timestamp : 0;
-  }
-
-  private async processTokenUpdateEvents(id: string, events: Event[]) {
-    const tokenUpdatedEvents = events.filter(
-      (event) => event.eventType == 'tokenUpdated' && event.data &&
-        event.data.token && event.data.token.registrationId);
-    if (tokenUpdatedEvents.length > 0) {
-      const token = tokenUpdatedEvents[tokenUpdatedEvents.length - 1].data.token.registrationId;
-      const existingCharacterWithThatToken =
-        await this.dbContainer().accountsDb().find({ selector: { pushToken: token } });
-      for (const existingCharacter of existingCharacterWithThatToken.docs) {
-        await this.dbContainer().accountsDb().upsert(existingCharacter._id, (accountInfo) => {
-          this.logger.info(`Removing token (${token} == ${accountInfo.pushToken}) from character ${existingCharacter._id} to give it to ${id}`)
-          delete accountInfo.pushToken;
-          return accountInfo;
-        });
-      }
-      await this.dbContainer().accountsDb().upsert(id, (accountInfo) => {
-        this.logger.info(`Saving push token`, { characterId: id, source: 'api' })
-        accountInfo.pushToken = token;
-        return accountInfo;
-      });
-    }
-    return events.filter((event) => event.eventType != 'tokenUpdated');
-  }
-
-  // Removes _RefreshModel events which are too far in future
-  // (timestamp > current timestamp + settings.tooFarInFutureFilterTime).
-  // Then removes all _RefreshModel events except latest one.
-  private filterRefreshModelEventsByTimestamp(id, events: Event[]): Event[] {
-    const tooFarInFuturetimestamp = currentTimestamp() + Container.get(ApplicationSettingsToken).tooFarInFutureFilterTime;
-    events = events.filter((value: any) =>
-      value.eventType != '_RefreshModel' || value.timestamp < tooFarInFuturetimestamp);
-    if (events.length == 0) {
-      this.logger.warn(`All received events are from the future!`, { characterId: id, source: 'api' });
-    }
-    const refreshModelEvents = events.filter((event) => event.eventType == '_RefreshModel');
-
-    const lastRefreshModelEventTimestamp =
-      Math.max(...refreshModelEvents.map((event) => event.timestamp));
-    return events.filter((value: any) =>
-      value.eventType != '_RefreshModel' || value.timestamp == lastRefreshModelEventTimestamp);
   }
 
   private async sendPushNotifications(timestampBefore: number, updatedViewModel: any): Promise<void> {
@@ -188,19 +92,15 @@ export class EventsController {
         }));
   }
 
-  private logHalfSuccessfulResponse(req: express.Request, eventTypesBefore: Event[],
-    eventTypesAfter: any[], status: number) {
+  private logHalfSuccessfulResponse(req: express.Request, eventTypes: Event[], status: number) {
     const logData = createLogData(req, status);
-    logData.eventTypesBefore = eventTypesBefore;
-    logData.eventTypesAfter = eventTypesAfter
+    logData.eventTypes = eventTypes;
     this.logger.error('Successfully put events into DB, but they were not processed in time', logData);
   }
 
-  private logSuccessfulResponse(req: express.Request, eventTypesBefore: Event[],
-    eventTypesAfter: Event[], status: number) {
+  private logSuccessfulResponse(req: express.Request, eventTypes: Event[], status: number) {
     const logData = createLogData(req, status);
-    logData.eventTypesBefore = eventTypesBefore;
-    logData.eventTypesAfter = eventTypesAfter
+    logData.eventTypes = eventTypes;
     this.logger.info('Successfully processed all events and answered back to client', logData);
   }
 }
---
+++ deus-server/events.processor.ts
@@ -0,0 +1,112 @@
+import { StatusAndBody, Connection } from "./connection";
+import { Container } from "typedi";
+import { DatabasesContainerToken } from "./services/db-container";
+import { HttpError } from "routing-controllers";
+import { ApplicationSettingsToken } from "./services/settings";
+import { currentTimestamp } from "./utils";
+import { LoggerToken } from "./services/logger";
+import { characterIdTimestampOnlyRefreshesView } from "./consts";
+
+export interface Event {
+  eventType: string;
+  timestamp: number;
+  data?: any;
+}
+
+export class EventsProcessor {
+  private logger = Container.get(LoggerToken);
+
+  public async process(id: string, events: Event[]): Promise<StatusAndBody> {
+    events = await this.processTokenUpdateEvents(id, events);
+    const isMobileClient = events.some((event) => event.eventType == '_RefreshModel');
+    if (isMobileClient) {
+      events = this.filterRefreshModelEventsByTimestamp(id, events);
+    }
+    const cutTimestamp = await this.cutTimestamp(id);
+    if (!isMobileClient && events.some((event) => event.timestamp <= cutTimestamp))
+      throw new HttpError(409, "Can't accept event with timestamp earlier than cut timestamp: they won't get processed!");
+    events = events.filter((value: any) => value.timestamp > cutTimestamp);
+    if (isMobileClient) {
+      if (this.dbContainer().connections.has(id))
+        throw new HttpError(429, 'Multiple connections from one client are not allowed');
+
+      this.dbContainer().connections.set(id, new Connection(this.dbContainer().eventsDb(),
+        Container.get(ApplicationSettingsToken).viewmodelUpdateTimeout));
+
+      const viewModelTimestampBefore = await this.latestExistingMobileEventTimestamp(id);
+      const s = await this.dbContainer().connections.get(id).processEvents(id, events);
+      this.dbContainer().connections.delete(id);
+      return s;
+    } else {
+      // In this case we don't need to subscribe for viewmodel updates or
+      // block other clients from connecting.
+      // So we don't add Connection to this.connections.
+      const connection = new Connection(this.dbContainer().eventsDb(), 0);
+      return await connection.processEvents(id, events);
+    }
+  };
+
+  public async cutTimestamp(id: string): Promise<number> {
+    const [currentViewmodelTimestamp, lastEventTimeStamp] = await Promise.all([
+      this.latestViewmodelTimestamp(id),
+      this.latestExistingMobileEventTimestamp(id)
+    ]);
+    return Math.max(currentViewmodelTimestamp, lastEventTimeStamp);
+  }
+
+  public async latestExistingMobileEventTimestamp(id: string): Promise<number> {
+    const docs = await this.dbContainer().eventsDb().query<any>(characterIdTimestampOnlyRefreshesView,
+      { include_docs: true, descending: true, endkey: [id], startkey: [id, {}], limit: 1 });
+    return docs.rows.length ? docs.rows[0].doc.timestamp : 0;
+  }
+
+  private dbContainer() {
+    return Container.get(DatabasesContainerToken);
+  }
+
+  private async processTokenUpdateEvents(id: string, events: Event[]) {
+    const tokenUpdatedEvents = events.filter(
+      (event) => event.eventType == 'tokenUpdated' && event.data &&
+        event.data.token && event.data.token.registrationId);
+    if (tokenUpdatedEvents.length > 0) {
+      const token = tokenUpdatedEvents[tokenUpdatedEvents.length - 1].data.token.registrationId;
+      const existingCharacterWithThatToken =
+        await this.dbContainer().accountsDb().find({ selector: { pushToken: token } });
+      for (const existingCharacter of existingCharacterWithThatToken.docs) {
+        await this.dbContainer().accountsDb().upsert(existingCharacter._id, (accountInfo) => {
+          this.logger.info(`Removing token (${token} == ${accountInfo.pushToken}) from character ${existingCharacter._id} to give it to ${id}`)
+          delete accountInfo.pushToken;
+          return accountInfo;
+        });
+      }
+      await this.dbContainer().accountsDb().upsert(id, (accountInfo) => {
+        this.logger.info(`Saving push token`, { characterId: id, source: 'api' })
+        accountInfo.pushToken = token;
+        return accountInfo;
+      });
+    }
+    return events.filter((event) => event.eventType != 'tokenUpdated');
+  }
+
+  // Removes _RefreshModel events which are too far in future
+  // (timestamp > current timestamp + settings.tooFarInFutureFilterTime).
+  // Then removes all _RefreshModel events except latest one.
+  private filterRefreshModelEventsByTimestamp(id, events: Event[]): Event[] {
+    const tooFarInFuturetimestamp = currentTimestamp() + Container.get(ApplicationSettingsToken).tooFarInFutureFilterTime;
+    events = events.filter((value: any) =>
+      value.eventType != '_RefreshModel' || value.timestamp < tooFarInFuturetimestamp);
+    if (events.length == 0) {
+      this.logger.warn(`All received events are from the future!`, { characterId: id, source: 'api' });
+    }
+    const refreshModelEvents = events.filter((event) => event.eventType == '_RefreshModel');
+
+    const lastRefreshModelEventTimestamp =
+      Math.max(...refreshModelEvents.map((event) => event.timestamp));
+    return events.filter((value: any) =>
+      value.eventType != '_RefreshModel' || value.timestamp == lastRefreshModelEventTimestamp);
+  }
+
+  private async latestViewmodelTimestamp(id: string): Promise<number> {
+    return (await this.dbContainer().viewModelDb('mobile').get(id)).timestamp;
+  }
+}
