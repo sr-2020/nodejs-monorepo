6f6b9978587dfae77fcd9925ce307800214a8e2f
Alexey Eremin <a.eremin.msu@gmail.com>
2018-05-25 00:10:22 +0200

    Answer 404 if patient not found

--- deus-server/controllers/medic.controller.ts
+++ deus-server/controllers/medic.controller.ts
@@ -20,57 +20,65 @@ export class MedicController {
   @Post("/medic/run_lab_tests/:id")
   async runLabTests( @CurrentUser() user: Account, @Param("id") id: string, @Body() req: RunLabTestsRequest,
     @Res() res: express.Response) {
-    id = await canonicalId(id);
-    await checkAccess(user, id);
-    const dbContainer = Container.get(DatabasesContainerToken);
-    const model = await dbContainer.modelsDb().get(req.patientId);
+    try {
+      id = await canonicalId(id);
+      await checkAccess(user, id);
+      const dbContainer = Container.get(DatabasesContainerToken);
+      const model = await dbContainer.modelsDb().get(req.patientId);
 
-    let timestamp = currentTimestamp();
+      let timestamp = currentTimestamp();
 
-    let events: Event[] = req.tests.map((test: string) => {
-      return {
-        eventType: 'medic-run-lab-test',
+      let events: Event[] = req.tests.map((test: string) => {
+        return {
+          eventType: 'medic-run-lab-test',
+          timestamp: timestamp++,
+          data: {
+            test,
+            model,
+          },
+        }
+      })
+      events.push({
+        eventType: '_RefreshModel',
         timestamp: timestamp++,
-        data: {
-          test,
-          model,
-        },
-      }
-    })
-    events.push({
-      eventType: '_RefreshModel',
-      timestamp: timestamp++,
-    });;
+      });;
 
-    const s = await new EventsProcessor().process(id, events);
-    res.status(s.status);
-    return s.body;
+      const s = await new EventsProcessor().process(id, events);
+      res.status(s.status);
+      return s.body;
+    } catch (e) {
+      returnCharacterNotFoundOrRethrow(e);
+    }
   }
 
   @Post("/medic/add_comment/:id")
   async addComment( @CurrentUser() user: Account, @Param("id") id: string, @Body() req: AddCommentRequest,
     @Res() res: express.Response) {
-    id = await canonicalId(id);
-    await checkAccess(user, id);
+    try {
+      id = await canonicalId(id);
+      await checkAccess(user, id);
 
-    const dbContainer = Container.get(DatabasesContainerToken);
-    const model = await dbContainer.modelsDb().get(req.patientId);
+      const dbContainer = Container.get(DatabasesContainerToken);
+      const model = await dbContainer.modelsDb().get(req.patientId);
 
-    let timestamp = currentTimestamp();
-    let events: Event[] = [{
-      eventType: 'medic-add-comment',
-      timestamp: timestamp++,
-      data: {
-        text: req.text,
-        model,
-      },
-    }, {
-      eventType: '_RefreshModel',
-      timestamp: timestamp++,
-    }];
+      let timestamp = currentTimestamp();
+      let events: Event[] = [{
+        eventType: 'medic-add-comment',
+        timestamp: timestamp++,
+        data: {
+          text: req.text,
+          model,
+        },
+      }, {
+        eventType: '_RefreshModel',
+        timestamp: timestamp++,
+      }];
 
-    const s = await new EventsProcessor().process(id, events);
-    res.status(s.status);
-    return s.body;
+      const s = await new EventsProcessor().process(id, events);
+      res.status(s.status);
+      return s.body;
+    } catch (e) {
+      returnCharacterNotFoundOrRethrow(e);
+    }
   }
 }
