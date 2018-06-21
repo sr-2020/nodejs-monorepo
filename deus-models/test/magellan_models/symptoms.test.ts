488f3edad9c1c7d7b36ddd97ea0dee28d6b6ec38
Alexey Eremin <a.eremin.msu@gmail.com>
2018-06-21 22:55:27 +0200

    Fix formatting

--- deus-models/test/magellan_models/symptoms.test.ts
+++ deus-models/test/magellan_models/symptoms.test.ts
@@ -4,10 +4,10 @@ import { expect } from "chai";
 
 describe('Symptoms helper', () => {
   it('Symtoms table have correct number of elements', () => {
-      for (const indice of systemsIndices()) {
-        const symptoms = systemToSymptoms.get(indice);
-        expect(symptoms).to.exist;
-        expect(symptoms).to.have.length(7 + 7);
-      }
+    for (const indice of systemsIndices()) {
+      const symptoms = systemToSymptoms.get(indice);
+      expect(symptoms).to.exist;
+      expect(symptoms).to.have.length(7 + 7);
+    }
   })
 });
