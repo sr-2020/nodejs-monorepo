3367f100051891f5eddbf3ebb76bf00e5b57ce97
Alexey Eremin <a.eremin.msu@gmail.com>
2018-06-24 17:41:02 +0200

    Fix notifications

--- deus-server/controllers/economy.controller.ts
+++ deus-server/controllers/economy.controller.ts
@@ -48,7 +48,7 @@ export class EconomyController {
       await sendGenericPushNotification(body.receiver,
         makeVisibleNotificationPayload(`Получен платеж: ${body.amount}`,
           `Отправитель платежа: ${body.sender}`
-            + body.description != undefined ? `, описание: "${body.description}"` : ''));
+            + (body.description != undefined ? `, описание: "${body.description}"` : '')));
       return {};
     } catch (e) {
       returnCharacterNotFoundOrRethrow(e);
