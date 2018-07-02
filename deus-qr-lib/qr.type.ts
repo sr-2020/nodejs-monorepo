6b58862208579c652d8ae2a194b1e9014f16614c
Alexey Eremin <a.eremin.msu@gmail.com>
2018-07-02 23:20:50 +0200

    Add more QrTypes

--- deus-qr-lib/qr.type.ts
+++ deus-qr-lib/qr.type.ts
@@ -5,6 +5,15 @@ export enum QrType {
   Implant = 2,
   InstantEffect = 3,
 
+  MagellanPill = 4,  // payload should be of [1,2,3,4,5,6] kind
+  EnterShip = 5,     // payload should contain ship id (number)
+  LeaveShip = 6,
+
+  SpaceSuitRefill = 7, // payload is <unique id>,<time in minutes>
+  SpaceSuitTakeOff = 8, // payload is life support system disinfection power
+
+  LabTerminalRefill = 20, // payload is <unique id>,<how many tests to add>
+
   Passport = 100,
-  Bill = 101
+  Bill = 101,
 }
