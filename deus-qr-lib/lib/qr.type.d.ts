a4fc5336cb341f9e183ac2de9e58b9894dc2eef9
Alexey Eremin <a.eremin.msu@gmail.com>
2018-07-02 23:24:46 +0200

    Add more QrTypes compiled

--- deus-qr-lib/lib/qr.type.d.ts
+++ deus-qr-lib/lib/qr.type.d.ts
@@ -3,6 +3,12 @@ export declare enum QrType {
     Pill = 1,
     Implant = 2,
     InstantEffect = 3,
+    MagellanPill = 4,
+    EnterShip = 5,
+    LeaveShip = 6,
+    SpaceSuitRefill = 7,
+    SpaceSuitTakeOff = 8,
+    LabTerminalRefill = 20,
     Passport = 100,
     Bill = 101,
 }
