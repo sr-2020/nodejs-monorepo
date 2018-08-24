03ebc0aca1733830d3b7272f2c0e8c0f62ce3b41
Alexey Eremin <a.eremin.msu@gmail.com>
2018-08-24 18:22:08 +0200

    Revert "Don't create sample data for humans"
    This reverts commit 6e311d4d5746d1f16f69875c36a9b8eea6133ca7.

--- deus-worker-manager/src/db_init/sync_db.ts
+++ deus-worker-manager/src/db_init/sync_db.ts
@@ -124,7 +124,7 @@ async function run(): Promise<void> {
     await Promise.all(dbNames.map(name => createDbIfNotExists(connection, name)));
     await createDesignDocs();
     await importCatalogs(connection, catalogsStorage, catalogs);
-    await createMedicSampleData();
+    await createSampleData();
 }
 
 run().then(() => console.log('Done!'));
