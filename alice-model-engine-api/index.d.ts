f0b1be4e2f21361c44bd273b390964381de03609
Alexey Eremin <a.eremin.msu@gmail.com>
2018-08-31 19:12:41 +0200

    Indent alice-model-engine with 2 spaces

--- alice-model-engine-api/index.d.ts
+++ alice-model-engine-api/index.d.ts
@@ -2,91 +2,91 @@ export type LogLevel = 'debug' | 'info' | 'notice' | 'warn' | 'error' | 'crit' |
 export type LogSource = 'default' | 'manager' | 'engine' | 'model';
 
 export interface CharacterlessEvent {
-    eventType: string,
-    timestamp: number,
-    data?: any
+  eventType: string,
+  timestamp: number,
+  data?: any
 }
 
 export interface Event extends CharacterlessEvent {
-    characterId: string,
+  characterId: string,
 }
 
 export type SyncEvent = Event & { eventType: '_RefreshModel' }
 
 export type Timer = {
-    name: string,
-    miliseconds: number,
-    eventType: string,
-    data: any
+  name: string,
+  miliseconds: number,
+  eventType: string,
+  data: any
 }
 
 export type Effect = {
-    enabled: boolean,
-    id: string,
-    class: string,
-    type: 'normal' | 'functional',
-    handler: string
+  enabled: boolean,
+  id: string,
+  class: string,
+  type: 'normal' | 'functional',
+  handler: string
 }
 
 export type Modifier = {
-    mID: string,
-    name?: string,
-    class?: string,
-    system?: string,
-    enabled: boolean,
-    effects: Effect[],
-    [key: string]: any
+  mID: string,
+  name?: string,
+  class?: string,
+  system?: string,
+  enabled: boolean,
+  effects: Effect[],
+  [key: string]: any
 }
 
 export type Condition = {
-    mID: string,
-    id: string,
-    class: string,
-    text: string,
-    details?: string,
-    group?: string,
-    level?: number
+  mID: string,
+  id: string,
+  class: string,
+  text: string,
+  details?: string,
+  group?: string,
+  level?: number
 }
 
 export type EngineContext = {
-    timestamp: number,
-    [key: string]: any
+  timestamp: number,
+  [key: string]: any
 }
 
 export type EngineMessageEvents = {
-    type: 'events'
-    context: any
-    events: Event[]
+  type: 'events'
+  context: any
+  events: Event[]
 }
 
 export type EngineMessageConfigure = {
-    type: 'configure'
-    data: any
+  type: 'configure'
+  data: any
 }
 
 export type EngineMessageAquired = {
-    type: 'aquired'
-    data: {
-        [db: string]: {
-            [id: string]: any
-        }
+  type: 'aquired'
+  data: {
+    [db: string]: {
+      [id: string]: any
     }
+  }
 }
 
 export type EngineMessage = EngineMessageEvents | EngineMessageConfigure | EngineMessageAquired;
 
 export type EngineResultOk = {
-    status: 'ok'
-    baseModel: any,
-    workingModel: any,
-    viewModels: { [base: string]: any },
-    aquired?: { [key: string]: any },
-    events?: Event[]
+  status: 'ok'
+  baseModel: any,
+  workingModel: any,
+  viewModels: { [base: string]: any },
+  aquired?: { [key: string]: any },
+  events?: Event[]
 }
 
 export type EngineResultError = {
-    status: 'error',
-    error: any
+  status: 'error',
+  error: any
 }
 
 export type EngineResult = EngineResultOk | EngineResultError;
@@ -94,87 +94,87 @@ export type EngineResult = EngineResultOk | EngineResultError;
 export type EngineReplyResult = EngineResult & { type: 'result' };
 
 export type EngineReplyLog = {
-    type: 'log'
-    source: string
-    level: LogLevel
-    msg: string
-    params: any[]
+  type: 'log'
+  source: string
+  level: LogLevel
+  msg: string
+  params: any[]
 }
 
 export type EngineReplyAquire = {
-    type: 'aquire'
-    keys: [string, string][]
+  type: 'aquire'
+  keys: [string, string][]
 }
 
 export type EngineReplyReady = {
-    type: 'ready'
+  type: 'ready'
 }
 
 export type EngineReply = EngineReplyReady | EngineReplyResult | EngineReplyLog | EngineReplyAquire;
 
 export interface ReadModelApiInterface {
-    model: any
+  model: any
 
-    getCatalogObject(catalog: string, id: string): any
+  getCatalogObject(catalog: string, id: string): any
 
-    getModifierById(id: string): Modifier | undefined
-    getModifiersByName(name: string): Modifier[]
-    getModifiersByClass(className: string): Modifier[]
-    getModifiersBySystem(systemName: string): Modifier[]
+  getModifierById(id: string): Modifier | undefined
+  getModifiersByName(name: string): Modifier[]
+  getModifiersByClass(className: string): Modifier[]
+  getModifiersBySystem(systemName: string): Modifier[]
 
-    getEffectsByName(name: string): Effect[]
-    getEffectsByClass(className: string): Effect[]
+  getEffectsByName(name: string): Effect[]
+  getEffectsByClass(className: string): Effect[]
 
-    getConditionById(id: string): Condition | undefined
-    getConditionsByClass(className: string): Condition[]
-    getConditionsByGroup(group: string): Condition[]
+  getConditionById(id: string): Condition | undefined
+  getConditionsByClass(className: string): Condition[]
+  getConditionsByGroup(group: string): Condition[]
 
-    getTimer(name: string): Timer | undefined
+  getTimer(name: string): Timer | undefined
 }
 
 export interface LogApiInterface {
-    debug(msg: string, additionalData?: any): void
-    info(msg: string, additionalData?: any): void
-    warn(msg: string, additionalData?: any): void
-    error(msg: string, additionalData?: any): void
+  debug(msg: string, additionalData?: any): void
+  info(msg: string, additionalData?: any): void
+  warn(msg: string, additionalData?: any): void
+  error(msg: string, additionalData?: any): void
 }
 
 export interface WriteModelApiInterface {
-    // If modifier.mID is not present, will generate new unique one.
-    // Returns added modifier.
-    addModifier(modifier: Modifier): Modifier
-    // Will do nothing if no modifier with such mID is present.
-    removeModifier(mID: string): this
+  // If modifier.mID is not present, will generate new unique one.
+  // Returns added modifier.
+  addModifier(modifier: Modifier): Modifier
+  // Will do nothing if no modifier with such mID is present.
+  removeModifier(mID: string): this
 
-    // If condition with same id is present, will do nothing
-    // and return existing condition.
-    // If condition.mID is not present, will generate new unique one.
-    // Returns added condition.
-    addCondition(condition: Condition): Condition
-    // Will do nothing if no condition with such id is present.
-    removeCondition(id: string): this
+  // If condition with same id is present, will do nothing
+  // and return existing condition.
+  // If condition.mID is not present, will generate new unique one.
+  // Returns added condition.
+  addCondition(condition: Condition): Condition
+  // Will do nothing if no condition with such id is present.
+  removeCondition(id: string): this
 
-    // Schedules delayed event for current character.
-    // name should be unique - in other case new timer will override existing one.
-    setTimer(name: string, miliseconds: number, eventType: string, data: any): this
-    // Cancels existing timer.
-    // NB: timer must exist!
-    removeTimer(name: string): this
+  // Schedules delayed event for current character.
+  // name should be unique - in other case new timer will override existing one.
+  setTimer(name: string, miliseconds: number, eventType: string, data: any): this
+  // Cancels existing timer.
+  // NB: timer must exist!
+  removeTimer(name: string): this
 
-    // Adds event to events queue of characterId. If characterId is null,
-    // event is send to currently processed character. Timestamp of event
-    // is "now", i.e. equals to timestamp of event currently being processed.
-    sendEvent(characterId: string | null, event: string, data: any): this
+  // Adds event to events queue of characterId. If characterId is null,
+  // event is send to currently processed character. Timestamp of event
+  // is "now", i.e. equals to timestamp of event currently being processed.
+  sendEvent(characterId: string | null, event: string, data: any): this
 }
 
 export interface PreprocessApiInterface extends ReadModelApiInterface, LogApiInterface {
-    aquire(db: string, id: string): this
+  aquire(db: string, id: string): this
 }
 
 export interface ViewModelApiInterface extends ReadModelApiInterface, LogApiInterface {
-    baseModel: any
+  baseModel: any
 }
 
 export interface ModelApiInterface extends ReadModelApiInterface, WriteModelApiInterface, LogApiInterface {
-    aquired(db: string, id: string): any
+  aquired(db: string, id: string): any
 }
