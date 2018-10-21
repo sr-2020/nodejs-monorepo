9468ae490fa0e088c1dcbac807a51610b27d5795
Alexey Eremin <a.eremin.msu@gmail.com>
2018-10-21 18:01:29 +0200

    Use library with better typings

--- telegram-bot/main.ts
+++ telegram-bot/main.ts
@@ -1,13 +1,30 @@
 import * as dotenv from 'dotenv';
-import * as teleBot from 'telebot';
+import * as TelegramBot from 'node-telegram-bot-api';
 
 dotenv.load();
 
-const bot = new teleBot({
-    token: process.env.TELEGRAM_BOT_TOKEN as string,
-    polling: {},
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN as string, {polling: true});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+  if (!match) return;
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
 });
 
-bot.on('text', (msg) => msg.reply.text(msg.text));
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
 
-bot.start();
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
