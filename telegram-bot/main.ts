53fc6cc138c43e947fed5ba24d0d09ec09d13a3c
Alexey Eremin <a.eremin.msu@gmail.com>
2018-10-21 19:33:39 +0200

    Example updatable message and inline keyboard
    https://alicelarp.atlassian.net/browse/ALICE-53

--- telegram-bot/main.ts
+++ telegram-bot/main.ts
@@ -3,28 +3,47 @@ import * as TelegramBot from 'node-telegram-bot-api';
 
 dotenv.load();
 
+let currentValue = 100;
+
+const m: TelegramBot.InlineKeyboardMarkup = {
+  inline_keyboard: [[
+    {
+      text: 'Decrease',
+      callback_data: '-1',
+    },
+    {
+      text: 'Increase',
+      callback_data: '+1',
+    },
+  ]],
+};
+
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN as string, {polling: true});
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN as string, { polling: true });
 
 // Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
+bot.onText(/\/echo (.+)/, async (msg, _match) => {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
-  if (!match) return;
-
   const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
   // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
+  const res = await bot.sendMessage(chatId, `Текущее значение: ${currentValue}`, { reply_markup: m });
+  console.log(JSON.stringify(res));
 });
 
 // Listen for any kind of message. There are different kinds of
 // messages.
 bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
+  console.log(`Received message: ${JSON.stringify(msg)}`);
+});
 
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+bot.on('callback_query', async (query) => {
+  console.log(`Received message: ${JSON.stringify(query)}`);
+  currentValue = currentValue + Number(query.data);
+  await bot.editMessageText(`Текущее значение: ${currentValue}`,
+    { chat_id: query.message.chat.id, message_id: query.message.message_id });
+  await bot.editMessageReplyMarkup(m,
+    { chat_id: query.message.chat.id, message_id: query.message.message_id });
+  await bot.answerCallbackQuery(query.id);
 });
