2337a9da31099661926d1fe08c165aa30967d96a
Alexey Eremin <a.eremin.msu@gmail.com>
2019-05-09 23:11:46 +0200

    Add TransactionRepository

---
+++ src/repositories/transaction.repository.ts
@@ -0,0 +1,15 @@
+import {DefaultCrudRepository} from '@loopback/repository';
+import {Transaction} from '../models';
+import {MySqlDataSource} from '../datasources';
+import {inject} from '@loopback/core';
+
+export class TransactionRepository extends DefaultCrudRepository<
+  Transaction,
+  typeof Transaction.prototype.id
+> {
+  constructor(
+    @inject('datasources.MySQL') dataSource: MySqlDataSource,
+  ) {
+    super(Transaction, dataSource);
+  }
+}
