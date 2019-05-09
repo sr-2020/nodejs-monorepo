d1f6862133516626d3330d809c7e9830ca8768c2
Alexey Eremin <a.eremin.msu@gmail.com>
2019-05-09 22:07:19 +0200

    Add Transaction model

---
+++ src/models/transaction.model.ts
@@ -0,0 +1,29 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Transaction extends Entity {
+  @property({type: 'number', id: true})
+  id?: number;
+
+  @property({type: 'date', required: true})
+  created_at: string;
+
+  @property({type: 'number', required: true})
+  sin_from: number;
+
+  @property({type: 'number', required: true})
+  sin_to: number;
+
+  @property({type: 'number', required: true})
+  amount: number;
+
+  @property({type: 'string'})
+  comment?: string;
+
+  @property({type: 'number'})
+  recurrent_payment_id?: number;
+
+  constructor(data?: Partial<Transaction>) {
+    super(data);
+  }
+}
