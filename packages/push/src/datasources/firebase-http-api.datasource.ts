40f6c0e10ddbb998a49c9b22fdd4601cfc8eb1ce
Alexey Eremin <a.eremin.msu@gmail.com>
2019-06-19 00:11:37 +0200

    Add link to Firebase documentation

--- packages/push/src/datasources/firebase-http-api.datasource.ts
+++ packages/push/src/datasources/firebase-http-api.datasource.ts
@@ -2,6 +2,8 @@ import {inject} from '@loopback/core';
 import {juggler} from '@loopback/repository';
 import * as config from './firebase-http-api.datasource.json';
 
+// See https://firebase.google.com/docs/cloud-messaging/http-server-ref
+// for more examples of payloads that can be send via push notification.
 export class FirebaseHttpApiDataSource extends juggler.DataSource {
   static dataSourceName = 'FirebaseHttpApi';
 
