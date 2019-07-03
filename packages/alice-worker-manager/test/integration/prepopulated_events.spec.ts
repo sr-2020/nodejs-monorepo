665210680727923645d1c857dfca50b8b7afd08b
Alexey Eremin <a.eremin.msu@gmail.com>
2019-07-03 20:40:37 +0200

    More timeout increases

--- packages/alice-worker-manager/test/integration/prepopulated_events.spec.ts
+++ packages/alice-worker-manager/test/integration/prepopulated_events.spec.ts
@@ -15,7 +15,7 @@ async function createAndStartManager(): Promise<Manager> {
 }
 
 describe('Prepopulated events', function() {
-  this.timeout(5000);
+  this.timeout(10000);
 
   let manager: Manager;
 
