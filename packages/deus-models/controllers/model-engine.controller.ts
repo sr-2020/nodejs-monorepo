30a52ec012231fe625f0918f813c84823af42d0f
Alexey Eremin <a.eremin.msu@gmail.com>
2019-07-05 20:14:15 +0200

    Fix some TODOs and improve documentation

--- packages/deus-models/controllers/model-engine.controller.ts
+++ packages/deus-models/controllers/model-engine.controller.ts
@@ -8,30 +8,52 @@ export class ModelEngineController {
   constructor(@inject(Engine.bindingKey) private _engine: Engine<DeusExModel>) {}
 
   @post('/process', {
-    // TODO: Document failure modes
+    summary: 'Calculates state of provided model at given timestamp in the future (compared to model timestamp) given some events.',
+    description: 'NB: This method is stateless. Caller should handle persistent storing of returned baseModel.',
     responses: {
       '200': {
-        description: 'Success',
+        description: 'Model processing is successfull. Response will contain calculated base/work model.',
         content: {
           'application/json': { schema: { 'x-ts-type': DeusExProcessModelResponse } },
         },
+      },
+      '422': {
+        description:
+          "There were problems with the request body. Either it doesn't conform to schema, or timestamp order is wrong. " +
+          'Required order is baseModel.timestamp < event.timestamp (for all events) < timestamp.',
+      },
+      '500': {
+        description:
+          'Model processing failed. Typical reasons are unsupported events, invalid event payloads, or exceptions in model scripts.',
       },
     },
   })
   // TODO: Add at least some simple test.
   async process(@requestBody() req: DeusExProcessModelRequest): Promise<DeusExProcessModelResponse> {
-    // TODO: Valdate timestamp order
+    req.events.forEach((event) => {
+      if (event.timestamp < req.baseModel.timestamp)
+        throw new HttpErrors.UnprocessableEntity(
+          `One of the events (${JSON.stringify(event)}) is in the past compared to model timestamp (${req.baseModel.timestamp})`,
+        );
+    });
+
+    req.events.forEach((event) => {
+      if (event.timestamp > req.timestamp)
+        throw new HttpErrors.UnprocessableEntity(
+          `One of the events (${JSON.stringify(event)}) is in the future compared to calculation timestamp (${req.timestamp})`,
+        );
+    });
+
     req.events.push({ eventType: '_', modelId: req.baseModel.modelId, timestamp: req.timestamp });
     // TODO: Support preprocess
     const aquired: AquiredObjects = {};
     const res = this._engine.process(req.baseModel, aquired, req.events);
 
-    // TODO: Better success validation
     // TODO: Also return viewmodels
     if (res.status == 'ok') {
       return { baseModel: res.baseModel, workModel: res.workingModel };
     } else {
-      throw new HttpErrors.BadRequest('Не шмогла');
+      throw new HttpErrors.InternalServerError(`Error during model processing: ${res.error}`);
     }
   }
 }
