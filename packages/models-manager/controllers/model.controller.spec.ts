86166979b56c9d08ed5542f076715f210a3d7e57
Alexey Eremin <a.eremin.msu@gmail.com>
2019-07-05 21:58:35 +0200

    Add test for models-manager GET /model (and fix it)

--- packages/models-manager/controllers/model.controller.spec.ts
+++ packages/models-manager/controllers/model.controller.spec.ts
@@ -1,10 +1,15 @@
 import { Client, expect } from '@loopback/testlab';
+import * as nock from 'nock';
 import { ModelsManagerApplication } from '../application';
 import { setupApplication } from '../test-helper';
 import { DeusExModelRepository } from '../repositories/deus-ex-model.repository';
-import * as model from '@sr2020/interface/models/deus-ex-model-example.json';
+import * as modelImport from '@sr2020/interface/models/deus-ex-model-example.json';
+import { DeusExModelDbEntity } from 'models-manager/models/deus-ex-model-db-entity';
+import { DeusExModel, DeusExProcessModelResponse } from '@sr2020/interface/models/deus-ex-model';
 
-describe('TransactionController', () => {
+const model = modelImport as DeusExModel;
+
+describe('ModelController', () => {
   let app: ModelsManagerApplication;
   let client: Client;
   let repo: DeusExModelRepository;
@@ -27,5 +32,18 @@ describe('TransactionController', () => {
     const allEntries = await repo.find();
     expect(allEntries.length).to.equal(1);
     expect(JSON.parse(allEntries[0].model)).to.containEql(model);
+  });
+
+  it('GET /model', async () => {
+    await repo.create(DeusExModelDbEntity.fromModel(model));
+
+    const r: DeusExProcessModelResponse = { baseModel: model, workModel: model };
+    const scope = nock('http://instance.evarun.ru:7006')
+      .post('/process')
+      .reply(200, r);
+
+    await client.get(`/model/${model.modelId}`).expect(200);
+
+    expect(scope.isDone()).to.be.true();
   });
 });
