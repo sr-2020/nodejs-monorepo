2bccd82804f88f23e7fed4eb03edfec413bfd584
Alexey Eremin <a.eremin.msu@gmail.com>
2019-07-19 18:57:43 +0200

    Add sqlite connection to the test fixture

--- packages/sr2020-models-testing/fixture.spec.ts
+++ packages/sr2020-models-testing/fixture.spec.ts
@@ -1,8 +1,14 @@
 import { Client } from '@loopback/testlab';
 import { ModelsManagerApplication } from '@sr2020/models-manager/application';
 import { setupApplication } from './fixture';
+import { getConnection } from 'typeorm';
+import { Sr2020Character } from '@sr2020/interface/models/sr2020-character.model';
+import { getDefaultCharacter } from '@sr2020/sr2020-models/testing/test-helper';
 
-describe('Fixture', () => {
+describe('Fixture', function() {
+  // tslint:disable-next-line: no-invalid-this
+  this.timeout(15000);
+
   let app: ModelsManagerApplication;
   let client: Client;
 
@@ -12,10 +18,23 @@ describe('Fixture', () => {
 
   afterEach(async () => {
     await app.stop();
+    await getConnection().close();
   });
 
-  it('Returns 200 on success', async () => {
+  it('Ping', async () => {
     await client.get('/ping').expect(200);
+  });
+
+  it('Put character', async () => {
+    const m: Sr2020Character = {
+      modelId: '1',
+      spellsCasted: 0,
+      ...getDefaultCharacter(),
+    };
+    await client
+      .put('/character/model')
+      .send(m)
+      .expect(200);
   });
 
   // TODO(aeremin): add more tests demonstrating fixture interaction
--- packages/sr2020-models-testing/fixture.ts
+++ packages/sr2020-models-testing/fixture.ts
@@ -1,20 +1,23 @@
-import { ModelsManagerApplication } from "@sr2020/models-manager/application";
-import {
-  createRestAppClient,
-  givenHttpServerConfig,
-  Client
-} from "@loopback/testlab";
+import { ModelsManagerApplication } from '@sr2020/models-manager/application';
+import { createRestAppClient, givenHttpServerConfig, Client } from '@loopback/testlab';
+import { createConnection } from 'typeorm';
+import { CharacterDbEntity } from 'models-manager/models/character-db-entity';
+import { LocationDbEntity } from 'models-manager/models/location-db-entity';
 
 export async function setupApplication(): Promise<AppWithClient> {
   const restConfig = givenHttpServerConfig({});
 
   const app = new ModelsManagerApplication({
-    rest: restConfig
+    rest: restConfig,
   });
 
   await app.boot();
   // TODO(aeremin): bind model engine service
-  // TODO(aeremin): create TypeORM connection to SQLite
+  await createConnection({
+    type: 'sqljs',
+    synchronize: true,
+    entities: [CharacterDbEntity, LocationDbEntity],
+  });
   await app.start();
 
   const client = createRestAppClient(app);
