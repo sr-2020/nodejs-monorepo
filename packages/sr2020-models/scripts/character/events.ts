75ea3eb949356cf6a4a2028b9a03262c74897b6b
Alexey Eremin <a.eremin.msu@gmail.com>
2019-08-31 14:39:24 +0200

    Allow to scan QRs on behalf of others

--- packages/sr2020-models/scripts/character/events.ts
+++ packages/sr2020-models/scripts/character/events.ts
@@ -1,5 +1,5 @@
 import { Event, UserVisibleError } from '@sr2020/interface/models/alice-model-engine';
-import { Sr2020CharacterApi } from '@sr2020/interface/models/sr2020-character.model';
+import { Sr2020CharacterApi, Sr2020Character } from '@sr2020/interface/models/sr2020-character.model';
 import { QrCode } from '@sr2020/interface/models/qr-code.model';
 import { consume } from '../qr/events';
 
@@ -9,11 +9,15 @@ export function consumeQrs(api: Sr2020CharacterApi, data: { qrCodes: number[] },
   }
 }
 
-export function scanQr(api: Sr2020CharacterApi, data: { qrCode: number; locationId?: string }, _: Event) {
+export function scanQr(api: Sr2020CharacterApi, data: { qrCode: number; locationId?: string; targetCharacterId?: number }, _: Event) {
   const qr: QrCode = api.aquired('QrCode', data.qrCode.toString());
   if (qr.type == 'empty') throw new UserVisibleError('Этот QR-код - пустышка, его нельзя использовать');
   if (!qr.eventType) throw new UserVisibleError('Этот QR-код нельзя использовать напрямую');
 
-  api.sendSelfEvent(qr.eventType, { ...qr.data, locationId: data.locationId });
+  if (data.targetCharacterId != undefined) {
+    api.sendOutboundEvent(Sr2020Character, data.targetCharacterId.toString(), qr.eventType, { ...qr.data, locationId: data.locationId });
+  } else {
+    api.sendSelfEvent(qr.eventType, { ...qr.data, locationId: data.locationId });
+  }
   api.sendOutboundEvent(QrCode, data.qrCode.toString(), consume, {});
 }
