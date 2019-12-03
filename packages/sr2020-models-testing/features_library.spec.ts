bf13fef181d4f0309f87fdcedf52c28decbeb856
Alexey Eremin <a.eremin.msu@gmail.com>
2019-12-04 00:58:15 +0100

    Add more sanity tests for features dictionary

--- packages/sr2020-models-testing/features_library.spec.ts
+++ packages/sr2020-models-testing/features_library.spec.ts
@@ -5,4 +5,27 @@ describe('Features library', () => {
   it('Not empty', async () => {
     expect(kAllFeatures).not.empty();
   });
+
+  it('Prerequisites are valid kAllFeatures keys', () => {
+    for (const [, feature] of kAllFeatures) {
+      for (const prereq of feature.prerequisites || []) {
+        expect(kAllFeatures.has(prereq)).to.be.true();
+      }
+    }
+  });
+
+  it('Not requiring itself', () => {
+    for (const [id, feature] of kAllFeatures) {
+      for (const prereq of feature.prerequisites || []) {
+        expect(prereq).not.equal(id);
+      }
+    }
+  });
+
+  it('Prerequisites are not duplicated', () => {
+    for (const [, feature] of kAllFeatures) {
+      const prereqs = feature.prerequisites || [];
+      expect(new Set<string>(prereqs).size).to.equal(prereqs.length);
+    }
+  });
 });
