6403eeb63a7ce5a98546c64f2f2e8720e5f31cab
Alexey Eremin <a.eremin.msu@gmail.com>
2019-12-05 22:44:48 +0100

    Support adding/removing features

--- packages/admin-ui/index.js
+++ packages/admin-ui/index.js
@@ -3,12 +3,39 @@ var app = new Vue({
   data: {
     desiredCharacterId: 128,
     characterModel: undefined,
+    allFeatures: undefined,
+    selectedFeature: 'magic-1',
+    modelsManagerUrl: 'http://instance.evarun.ru:7007/character/model/'
+  },
+  async created() {
+    const response = await this.$http.get(`http://instance.evarun.ru:7006/features`);
+    this.allFeatures = response.body;
   },
   methods: {
+    url(id) {
+      return `http://instance.evarun.ru:7007/character/model/${id}`;
+    },
+
     async chooseCharacter() {
-      const response = await this.$http.get(`http://instance.evarun.ru:7007/character/model/${this.desiredCharacterId}`);
-      response.body.workModel.features.forEach((f) => delete f.modifierIds);
-      this.characterModel = response.body.workModel;
-    }
+      const response = await this.$http.get(this.url(this.desiredCharacterId));
+      this.setCharacterModel(response.body.workModel);
+    },
+
+    setCharacterModel(model) {
+      model.features.forEach((f) => delete f.modifierIds);
+      this.characterModel = model;
+    },
+
+    async addFeature() {
+      const response = await this.$http.post(this.url(this.characterModel.modelId),
+        { eventType: 'addFeature', data: { id: this.selectedFeature } });
+      this.setCharacterModel(response.body.workModel);
+    },
+
+    async removeFeature() {
+      const response = await this.$http.post(this.url(this.characterModel.modelId),
+        { eventType: 'removeFeature', data: { id: this.selectedFeature } });
+      this.setCharacterModel(response.body.workModel);
+    },
   }
 })
