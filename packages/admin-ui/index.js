2d6024fc94a8969f149706c146cc86a8b7cd8883
Alexey Eremin <a.eremin.msu@gmail.com>
2019-12-05 21:55:42 +0100

    Create page showing basic stats of chosen character

--- packages/admin-ui/index.js
+++ packages/admin-ui/index.js
@@ -1,6 +1,13 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    desiredCharacterId: 128,
+    characterModel: undefined,
+  },
+  methods: {
+    async chooseCharacter() {
+      const response = await this.$http.get(`http://instance.evarun.ru:7007/character/model/${this.desiredCharacterId}`);
+      this.characterModel = response.body.workModel;
+    }
   }
 })
