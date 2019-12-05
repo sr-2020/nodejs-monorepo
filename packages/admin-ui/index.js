2f6f1519a8e84c6ed1878df4b2f1012391c047a7
Alexey Eremin <a.eremin.msu@gmail.com>
2019-12-05 22:12:49 +0100

    Show features

--- packages/admin-ui/index.js
+++ packages/admin-ui/index.js
@@ -7,6 +7,7 @@ var app = new Vue({
   methods: {
     async chooseCharacter() {
       const response = await this.$http.get(`http://instance.evarun.ru:7007/character/model/${this.desiredCharacterId}`);
+      response.body.workModel.features.forEach((f) => delete f.modifierIds);
       this.characterModel = response.body.workModel;
     }
   }
