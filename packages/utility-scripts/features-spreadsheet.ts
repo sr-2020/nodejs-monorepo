42c36a04cad3f4b80e8aff12c758284874f057c2
Alexey Eremin <a.eremin.msu@gmail.com>
2020-02-16 21:03:10 +0100

    Refactor import script to simplify support of other entities

--- packages/utility-scripts/features-spreadsheet.ts
+++ packages/utility-scripts/features-spreadsheet.ts
@@ -14,57 +14,75 @@ interface PassiveAbility {
   gmDescription: string;
 }
 
-async function main() {
-  const db = new Firestore();
-  const passiveAbilitiesRef = db.collection('passive_abilities');
+const db = new Firestore();
 
-  const auth = await google.auth.getClient({
-    scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
-  });
+class SpreadsheetProcessor {
+  passiveAbilitiesRef = db.collection('passive_abilities');
+  passiveAbilities: PassiveAbility[] = [];
 
-  // https://docs.google.com/spreadsheets/d/1G-GrHGf-iNp9YDOiPe97EkgY4g7FZbu_YfWO5TS_Q6A
-  const spreadsheetId = '1G-GrHGf-iNp9YDOiPe97EkgY4g7FZbu_YfWO5TS_Q6A';
-  const response = await sheets.spreadsheets.values.get({ auth, spreadsheetId, range: 'Фичи!A1:AE700' });
-  const data = response.data.values;
-  if (!data) {
-    throw new Error('Failed to get spreadsheet range');
+  async processPassiveAbility(line: number, id: string, row: any[]) {
+    const ability: PassiveAbility = {
+      id,
+      name: row[6],
+      description: row[13],
+      gmDescription: row[14],
+      originalLine: line + 1,
+    };
+    const existingDoc = await this.passiveAbilitiesRef.doc(id).get();
+    if (
+      existingDoc.data()?.name != ability.name ||
+      existingDoc.data()?.description != ability.description ||
+      existingDoc.data()?.gmDescription != ability.gmDescription
+    ) {
+      this.passiveAbilities.push(ability);
+    }
   }
 
-  for (let r = 1; r < 600; ++r) {
-    const row = data[r];
-    const id = row[5];
-    const kind = row[0];
-    if (id && kind == 'Пассивная абилка') {
-      const ability: PassiveAbility = {
-        id,
-        name: row[6],
-        description: row[13],
-        gmDescription: row[14],
-        originalLine: r + 1,
-      };
-      const existingDoc = await passiveAbilitiesRef.doc(id).get();
-      if (
-        existingDoc.data()?.name != ability.name ||
-        existingDoc.data()?.description != ability.description ||
-        existingDoc.data()?.gmDescription != ability.gmDescription
-      ) {
-        console.log(`
-          {
-            id: '${ability.id}',
-            name: '${ability.name}',
-            description: '${ability.description.replace(/\n/g, '\\n')}',
-            // ${ability.originalLine}
-            // ${ability.gmDescription.replace(/\n/g, '\n          // ')}
-            // TODO(aeremin): Implement and add modifier here
-            modifier: [],
-          },`);
-      }
-      await passiveAbilitiesRef.doc(id).set(ability);
+  async printAndSavePassiveAbilities() {
+    console.log('//*************************** Passive abilities ***************************//');
+    for (const ability of this.passiveAbilities) {
+      console.log(`
+      {
+        id: '${ability.id}',
+        name: '${ability.name}',
+        description: '${ability.description.replace(/\n/g, '\\n')}',
+        // ${ability.originalLine}
+        // ${ability.gmDescription.replace(/\n/g, '\n          // ')}
+        // TODO(aeremin): Implement and add modifier here
+        modifier: [],
+      },`);
+      await this.passiveAbilitiesRef.doc(ability.id).set(ability);
     }
+  }
+
+  async run() {
+    const auth = await google.auth.getClient({
+      scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+    });
+
+    // https://docs.google.com/spreadsheets/d/1G-GrHGf-iNp9YDOiPe97EkgY4g7FZbu_YfWO5TS_Q6A
+    const spreadsheetId = '1G-GrHGf-iNp9YDOiPe97EkgY4g7FZbu_YfWO5TS_Q6A';
+    const response = await sheets.spreadsheets.values.get({ auth, spreadsheetId, range: 'Фичи!A1:AE700' });
+    const data = response.data.values;
+    if (!data) {
+      throw new Error('Failed to get spreadsheet range');
+    }
+
+    for (let r = 1; r < 600; ++r) {
+      const row = data[r];
+      const id = row[5];
+      const kind = row[0];
+
+      if (kind == 'Пассивная абилка') {
+        if (!id) throw new Error(`Entity in the line ${r + 1} has no ID`);
+        await this.processPassiveAbility(r, id, row);
+      }
+    }
+    this.printAndSavePassiveAbilities();
   }
 }
 
-main().then(
+new SpreadsheetProcessor().run().then(
   () => console.log('Finished OK'),
   (e) => console.log(`Finished with error: ${e}`),
 );
