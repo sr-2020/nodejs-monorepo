3008850d98c598ec7f3399b743ec34b45abacefb
Alexey Eremin <a.eremin.msu@gmail.com>
2020-02-20 22:18:30 +0100

    Add createMerchandise QR event

--- packages/sr2020-models/scripts/qr/events.ts
+++ packages/sr2020-models/scripts/qr/events.ts
@@ -21,3 +21,29 @@ export function create(api: EventModelApi<QrCode>, data: Partial<QrCode>, _: Eve
 
   api.model = { ...api.model, ...data, timestamp: api.model.timestamp, modelId: api.model.modelId, modifiers: [], timers: undefined };
 }
+
+interface Merchandise {
+  id: string;
+  name: string;
+  description: string;
+  numberOfUses?: number;
+  additionalData: any;
+}
+
+export function createMerchandise(api: EventModelApi<QrCode>, data: Merchandise, _: Event) {
+  if (api.model.type != 'empty') {
+    throw new UserVisibleError('QR-код уже записан!');
+  }
+
+  api.model = {
+    type: 'merchandise',
+    name: data.name,
+    description: data.description,
+    usesLeft: data.numberOfUses ?? 1,
+    data: { ...data.additionalData, id: data.id },
+
+    timestamp: api.model.timestamp,
+    modifiers: [],
+    modelId: api.model.modelId,
+  };
+}
