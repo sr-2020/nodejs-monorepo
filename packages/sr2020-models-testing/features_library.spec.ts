79a3b54c904fdf7d22cb215394e528f7758846e2
Alexey Eremin <a.eremin.msu@gmail.com>
2020-02-21 20:20:13 +0100

    Improve feature library tests

--- packages/sr2020-models-testing/features_library.spec.ts
+++ packages/sr2020-models-testing/features_library.spec.ts
@@ -1,20 +1,30 @@
 import { expect } from '@loopback/testlab';
+import { kAllActiveAbilities } from '@sr2020/sr2020-models/scripts/character/active_abilities_library';
 import { kAllPassiveAbilities } from '@sr2020/sr2020-models/scripts/character/passive_abilities_library';
+import { kAllSpells } from '@sr2020/sr2020-models/scripts/character/spells_library';
 
 describe('Features library', () => {
-  it('Not empty', async () => {
+  const allValidPrerequisites = new Set<string>([...kAllPassiveAbilities.keys(), ...kAllActiveAbilities.keys(), ...kAllSpells.keys()]);
+
+  it('Not empty', () => {
     expect(kAllPassiveAbilities).not.empty();
+    expect(kAllActiveAbilities).not.empty();
+    expect(kAllSpells).not.empty();
   });
 
-  it('Prerequisites are valid kAllPassiveAbilities keys', () => {
+  it('No id duplication between different kinds of features', () => {
+    expect(allValidPrerequisites.size).to.equal(kAllPassiveAbilities.size + kAllActiveAbilities.size + kAllSpells.size);
+  });
+
+  it('Passive abilities prerequisites are valid', () => {
     for (const [, feature] of kAllPassiveAbilities) {
       for (const prereq of feature.prerequisites ?? []) {
-        expect(kAllPassiveAbilities.has(prereq)).to.be.true();
+        expect(allValidPrerequisites.has(prereq)).to.be.true();
       }
     }
   });
 
-  it('Not requiring itself', () => {
+  it('Passive ability does not require itself', () => {
     for (const [id, feature] of kAllPassiveAbilities) {
       for (const prereq of feature.prerequisites ?? []) {
         expect(prereq).not.equal(id);
@@ -22,8 +32,54 @@ describe('Features library', () => {
     }
   });
 
-  it('Prerequisites are not duplicated', () => {
+  it('Passive ability prerequisites are not duplicated', () => {
     for (const [, feature] of kAllPassiveAbilities) {
+      const prereqs = feature.prerequisites ?? [];
+      expect(new Set<string>(prereqs).size).to.equal(prereqs.length);
+    }
+  });
+
+  it('Active abilities prerequisites are valid', () => {
+    for (const [, feature] of kAllActiveAbilities) {
+      for (const prereq of feature.prerequisites ?? []) {
+        expect(allValidPrerequisites.has(prereq)).to.be.true();
+      }
+    }
+  });
+
+  it('Active ability does not require itself', () => {
+    for (const [id, feature] of kAllActiveAbilities) {
+      for (const prereq of feature.prerequisites ?? []) {
+        expect(prereq).not.equal(id);
+      }
+    }
+  });
+
+  it('Active ability prerequisites are not duplicated', () => {
+    for (const [, feature] of kAllActiveAbilities) {
+      const prereqs = feature.prerequisites ?? [];
+      expect(new Set<string>(prereqs).size).to.equal(prereqs.length);
+    }
+  });
+
+  it('Spells prerequisites are valid', () => {
+    for (const [, feature] of kAllSpells) {
+      for (const prereq of feature.prerequisites ?? []) {
+        expect(allValidPrerequisites.has(prereq)).to.be.true();
+      }
+    }
+  });
+
+  it('Spell does not require itself', () => {
+    for (const [id, feature] of kAllSpells) {
+      for (const prereq of feature.prerequisites ?? []) {
+        expect(prereq).not.equal(id);
+      }
+    }
+  });
+
+  it('Spells prerequisites are not duplicated', () => {
+    for (const [, feature] of kAllSpells) {
       const prereqs = feature.prerequisites ?? [];
       expect(new Set<string>(prereqs).size).to.equal(prereqs.length);
     }
