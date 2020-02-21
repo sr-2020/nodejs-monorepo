efdd7ffa7e6877b5230dabfc141c146d695b95b1
Alexey Eremin <a.eremin.msu@gmail.com>
2020-02-21 20:55:57 +0100

    Implement generic useAbility event

---
+++ packages/sr2020-models/scripts/character/active_abilities.ts
@@ -0,0 +1,18 @@
+import { EventModelApi, UserVisibleError, Event } from '@sr2020/interface/models/alice-model-engine';
+import { Sr2020Character } from '@sr2020/interface/models/sr2020-character.model';
+
+interface ActiveAbilityData {
+  id: string; // corresponds to ActiveAbility.id and AddedActiveAbility.id
+  targetCharacterId?: string; // Identifier of target character
+}
+
+export function useAbility(api: EventModelApi<Sr2020Character>, data: ActiveAbilityData, event: Event) {
+  const ability = api.workModel.activeAbilities.find((s) => s.id == data.id);
+  if (!ability) {
+    throw new UserVisibleError('Нельзя использовать способностей, которой у вас нет!');
+  }
+
+  // TODO(aeremin) Handle cooldown
+
+  api.sendSelfEvent(ability.eventType, data);
+}
