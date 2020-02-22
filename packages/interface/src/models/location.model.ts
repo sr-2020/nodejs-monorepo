b38556e59560e6805a0dd353e04bf52a8300e640
Alexey Eremin <a.eremin.msu@gmail.com>
2020-02-22 14:23:36 +0100

    Record gender & metarace to spell trace

--- packages/interface/src/models/location.model.ts
+++ packages/interface/src/models/location.model.ts
@@ -8,6 +8,8 @@ export class SpellTrace {
   @rproperty() timestamp: number;
   @rproperty() spellName: string;
   @rproperty() casterAura: string;
+  @rproperty() gender: string;
+  @rproperty() metarace: string;
   @rproperty() power: number;
   @rproperty() magicFeedback: number;
 }
