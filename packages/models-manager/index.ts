ef5fe6a4c7e500f649c568a997b7fdaec4c21ea2
Alexey Eremin <a.eremin.msu@gmail.com>
2020-03-16 21:31:49 +0100

    CI Test

--- packages/models-manager/index.ts
+++ packages/models-manager/index.ts
@@ -15,7 +15,7 @@ export async function main(options: ApplicationConfig = {}) {
   await app.start();
 
   const url = app.restServer.url;
-  console.log(`Models manager is running at ${url}`);
+  console.log(`Server is running at ${url}`);
   console.log(`Try ${url}/ping`);
 
   return app;
