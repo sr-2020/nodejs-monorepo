e77444c69a9e4a42d68e30775d1c13d1ee07b848
Alexey Eremin <a.eremin.msu@gmail.com>
2020-03-23 00:50:49 +0100

    Fix test fixture issue and update @loopback/rest
    It seems now validation is a bit stricter

--- packages/sr2020-models-testing/fixture.ts
+++ packages/sr2020-models-testing/fixture.ts
@@ -133,7 +133,10 @@ export class TestFixture {
 
   async saveCharacter(model: Partial<Sr2020Character> = {}) {
     const id = model.modelId ?? 0;
-    await this.client.put(`/character/default/${id}?noAbilities=true`).expect(200);
+    await this.client
+      .put(`/character/default/${id}?noAbilities=true`)
+      .send({})
+      .expect(200);
     const character = await this._connection.getRepository(Sr2020Character).findOneOrFail(id);
     await this._connection.getRepository(Sr2020Character).save({ ...character, ...model });
   }
