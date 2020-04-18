adf4489b744ce4b3503ba5f0e07a66547256dae4
Alexey Eremin <a.eremin.msu@gmail.com>
2020-04-18 19:39:07 +0200

    Create HMHVV hunger timer

--- packages/sr2020-models/scripts/character/races.ts
+++ packages/sr2020-models/scripts/character/races.ts
@@ -1,6 +1,23 @@
 import { MetaRace, Sr2020Character } from '@sr2020/interface/models/sr2020-character.model';
 import { EventModelApi, Event } from '@sr2020/interface/models/alice-model-engine';
+import { duration } from 'moment';
+
+const kHmhvvHungerTimer = 'hmhvv-hunger';
+const kHmhvvHungerPeriod = duration(1, 'hour');
+const kEssenceLostPerHungerTick = 50;
 
 export function setRace(api: EventModelApi<Sr2020Character>, data: { race: MetaRace }, _: Event) {
   api.model.metarace = data.race;
+
+  if (api.model.metarace == 'meta-hmhvv1' || api.model.metarace == 'meta-hmhvv3') {
+    api.setTimer(kHmhvvHungerTimer, kHmhvvHungerPeriod, hungerTick, {});
+  } else {
+    api.removeModifier(kHmhvvHungerTimer);
+  }
+}
+
+export function hungerTick(api: EventModelApi<Sr2020Character>, data: {}, _: Event) {
+  api.setTimer(kHmhvvHungerTimer, kHmhvvHungerPeriod, hungerTick, {});
+  api.model.essenceDetails.gap += Math.min(kEssenceLostPerHungerTick, api.workModel.essence);
+  api.sendNotification('Голод HMHVV', 'Вы испытываете нечеловеческий голод');
 }
