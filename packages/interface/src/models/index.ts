1ca149648d96242f6a6bb4b847e985b76aab5af4
Alexey Eremin <a.eremin.msu@gmail.com>
2020-04-20 00:24:59 +0200

    Remove unused code

--- packages/interface/src/models/index.ts
+++ packages/interface/src/models/index.ts
@@ -1,7 +1,4 @@
-export * from './transaction.model';
-export * from './transfer.model';
 export * from './empty.model';
-export * from './account-info.model';
 export * from './firebase-token.model';
 export * from './push-notification.model';
 export * from './push-result.model';
