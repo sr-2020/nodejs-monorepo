e1b8ed5321592d17e5b0703b8f612667c3aedacf
Alexey Eremin <a.eremin.msu@gmail.com>
2020-04-20 14:29:35 +0200

    Provide an env variable to add pubsub topic prefix and optionally disable pubsub completely

--- packages/models-manager/services/pubsub.service.ts
+++ packages/models-manager/services/pubsub.service.ts
@@ -1,6 +1,9 @@
 import { PubSub } from '@google-cloud/pubsub';
 import { Provider } from '@loopback/core';
 
+const kEnablePubSub: boolean = process.env.ENABLE_PUBSUB != 'false';
+const kPubSubTopicPrefix: string = process.env.PUBSUB_TOPIC_PREFIX ?? '';
+
 export interface PubSubService {
   publish(topic: string, message: any): Promise<string>;
 }
@@ -9,12 +12,18 @@ export class PubSubServiceImpl implements PubSubService {
   static readonly pubsub = new PubSub();
 
   publish(topic: string, message: any): Promise<string> {
-    return PubSubServiceImpl.pubsub.topic(topic).publishJSON(message);
+    return PubSubServiceImpl.pubsub.topic(kPubSubTopicPrefix + topic).publishJSON(message);
+  }
+}
+
+export class NoOpPubSubServiceImpl implements PubSubService {
+  publish(topic: string, message: any): Promise<string> {
+    return Promise.resolve('');
   }
 }
 
 export class PubSubServiceProvider implements Provider<PubSubService> {
   value(): PubSubService {
-    return new PubSubServiceImpl();
+    return kEnablePubSub ? new PubSubServiceImpl() : new NoOpPubSubServiceImpl();
   }
 }
