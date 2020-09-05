4a3bbb224c0f9f9a8ddaab8515a177f3e7024110
Alexey Eremin <a.eremin.msu@gmail.com>
2020-09-05 18:53:39 +0200

    Switch back to port 3000 as some other configs reference it

--- packages/sr2020-model-engine/index.ts
+++ packages/sr2020-model-engine/index.ts
@@ -18,7 +18,7 @@ export async function main(options: ApplicationConfig = {}) {
 
 const config = {
   rest: {
-    port: +(process.env.PORT ?? 3001),
+    port: +(process.env.PORT ?? 3000),
     host: process.env.HOST,
     openApiSpec: {
       // useful when used with OASGraph to locate your application
