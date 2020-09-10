6656ab79888be09187841fe680025660ac9297a1
Alexey Eremin <a.eremin.msu@gmail.com>
2020-09-11 00:32:09 +0200

    Reduce code duplication in reparser

--- packages/utility-scripts/reparser.ts
+++ packages/utility-scripts/reparser.ts
@@ -74,28 +74,24 @@ function addComment(node: ts.Node, comment: string) {
   }
 }
 
-export function rewritePassiveAbilities(abilities: PassiveAbility[]) {
-  const file = readSourceFileWithoutComments(PASSIVE_ABILITIES_FILENAME);
+type AnyAbility = PassiveAbility | ActiveAbility | Spell;
 
-  const transformer = (context: ts.TransformationContext) => (rootNode: ts.SourceFile): ts.SourceFile => {
-    let currentAbility: PassiveAbility | undefined = undefined;
+function createTransformer<T extends AnyAbility>(
+  listName: string,
+  abilities: T[],
+  abilityToObjectLiteral: (ability: T) => ts.ObjectLiteralExpression,
+  abilityPropertyToLiteral: (ability: T, propertyName: string) => ts.LiteralExpression,
+  spreadsheetProperties: string[],
+) {
+  return (context: ts.TransformationContext) => (rootNode: ts.SourceFile): ts.SourceFile => {
+    let currentAbility: T | undefined = undefined;
+
     const abilityVisit = (node: ts.Node): ts.Node => {
       if (ts.isArrayLiteralExpression(node)) {
         const existingAbilities = ts.visitEachChild(node, abilityVisit, context);
         const elements: ts.Expression[] = [...existingAbilities.elements];
         for (const ability of abilities) {
-          const element = ts.createObjectLiteral(
-            [
-              ts.createPropertyAssignment(ts.createIdentifier('id'), ts.createStringLiteral(ability.id)),
-              ts.createPropertyAssignment(ts.createIdentifier('name'), ts.createStringLiteral(ability.name)),
-              ts.createPropertyAssignment(ts.createIdentifier('description'), ts.createStringLiteral(ability.description)),
-              ts.createPropertyAssignment(ts.createIdentifier('karmaCost'), ts.createNumericLiteral(ability.karmaCost)),
-              ts.createPropertyAssignment(ts.createIdentifier('prerequisites'), ts.createArrayLiteral([])),
-              ts.createPropertyAssignment(ts.createIdentifier('modifier'), ts.createArrayLiteral([])),
-            ],
-            true,
-          );
-          addComment(element, 'TODO(aeremin): Implement and add modifier here');
+          const element = abilityToObjectLiteral(ability);
           addComment(element, ability.gmDescription);
           elements.push(element);
         }
@@ -120,18 +116,10 @@ export function rewritePassiveAbilities(abilities: PassiveAbility[]) {
           }
         }
 
-        if (currentAbility) {
-          if (propertyName == 'name') {
-            return ts.createPropertyAssignment('name', ts.createStringLiteral(currentAbility.name));
-          }
-          if (propertyName == 'description') {
-            return ts.createPropertyAssignment('description', ts.createStringLiteral(currentAbility.description));
-          }
-          if (propertyName == 'karmaCost') {
-            return ts.createPropertyAssignment('karmaCost', ts.createNumericLiteral(currentAbility.karmaCost));
-          }
+        if (currentAbility && spreadsheetProperties.includes(propertyName)) {
+          return ts.createPropertyAssignment(propertyName, abilityPropertyToLiteral(currentAbility, propertyName));
         }
-        if (propertyName == 'modifier' || propertyName == 'prerequisites' || propertyName == 'prerequisites') {
+        if (!spreadsheetProperties.includes(propertyName)) {
           return node;
         }
       }
@@ -139,7 +127,7 @@ export function rewritePassiveAbilities(abilities: PassiveAbility[]) {
     };
 
     const topLevelVisit = (node: ts.Node): ts.Node => {
-      if (ts.isVariableDeclaration(node) && getName(node) == 'kAllPassiveAbilitiesList') {
+      if (ts.isVariableDeclaration(node) && getName(node) == listName) {
         const result = ts.visitEachChild(node, abilityVisit, context);
         return result;
       } else {
@@ -149,98 +137,87 @@ export function rewritePassiveAbilities(abilities: PassiveAbility[]) {
 
     return ts.visitNode(rootNode, topLevelVisit);
   };
+}
+
+export function rewritePassiveAbilities(abilities: PassiveAbility[]) {
+  const file = readSourceFileWithoutComments(PASSIVE_ABILITIES_FILENAME);
+
+  const transformer = createTransformer(
+    'kAllPassiveAbilitiesList',
+    abilities,
+    (ability) => {
+      const element = ts.createObjectLiteral(
+        [
+          ts.createPropertyAssignment(ts.createIdentifier('id'), ts.createStringLiteral(ability.id)),
+          ts.createPropertyAssignment(ts.createIdentifier('name'), ts.createStringLiteral(ability.name)),
+          ts.createPropertyAssignment(ts.createIdentifier('description'), ts.createStringLiteral(ability.description)),
+          ts.createPropertyAssignment(ts.createIdentifier('karmaCost'), ts.createNumericLiteral(ability.karmaCost)),
+          ts.createPropertyAssignment(ts.createIdentifier('prerequisites'), ts.createArrayLiteral([])),
+          ts.createPropertyAssignment(ts.createIdentifier('modifier'), ts.createArrayLiteral([])),
+        ],
+        true,
+      );
+      addComment(element, 'TODO(aeremin): Implement and add modifier here');
+      return element;
+    },
+    (ability, propertyName) => {
+      if (propertyName == 'name') {
+        return ts.createStringLiteral(ability.name);
+      }
+      if (propertyName == 'description') {
+        return ts.createStringLiteral(ability.description);
+      }
+      if (propertyName == 'karmaCost') {
+        return ts.createNumericLiteral(ability.karmaCost);
+      }
+      throw new Error(`Unexpected property name: ${propertyName}`);
+    },
+    ['name', 'description', 'karmaCost'],
+  );
   writeSourceFile(ts.transform(file, [transformer]).transformed[0], PASSIVE_ABILITIES_FILENAME);
 }
 
 export function rewriteActiveAbilities(abilities: ActiveAbility[]) {
   const file = readSourceFileWithoutComments(ACTIVE_ABILITIES_FILENAME);
 
-  const transformer = (context: ts.TransformationContext) => (rootNode: ts.SourceFile): ts.SourceFile => {
-    let currentAbility: ActiveAbility | undefined = undefined;
-    const abilityVisit = (node: ts.Node): ts.Node => {
-      if (ts.isArrayLiteralExpression(node)) {
-        const existingAbilities = ts.visitEachChild(node, abilityVisit, context);
-        const elements: ts.Expression[] = [...existingAbilities.elements];
-        for (const ability of abilities) {
-          const element = ts.createObjectLiteral(
-            [
-              ts.createPropertyAssignment(ts.createIdentifier('id'), ts.createStringLiteral(ability.id)),
-              ts.createPropertyAssignment(ts.createIdentifier('humanReadableName'), ts.createStringLiteral(ability.humanReadableName)),
-              ts.createPropertyAssignment(ts.createIdentifier('description'), ts.createStringLiteral(ability.description)),
-              ts.createPropertyAssignment(ts.createIdentifier('target'), ts.createStringLiteral('scan')),
-              ts.createPropertyAssignment(ts.createIdentifier('targetsSignature'), ts.createIdentifier('kNoTarget')),
-              ts.createPropertyAssignment(ts.createIdentifier('cooldownMinutes'), ts.createNumericLiteral(ability.cooldown)),
-              ts.createPropertyAssignment(ts.createIdentifier('karmaCost'), ts.createNumericLiteral(ability.karmaCost)),
-              ts.createPropertyAssignment(ts.createIdentifier('minimalEssence'), ts.createNumericLiteral(0)),
-              ts.createPropertyAssignment(
-                ts.createIdentifier('eventType'),
-                ts.createPropertyAccess(ts.createIdentifier('dummyAbility'), ts.createIdentifier('name')),
-              ),
-            ],
-            true,
-          );
-
-          addComment(element, 'TODO(aeremin): Add proper implementation');
-          addComment(element, ability.gmDescription);
-          elements.push(element);
-        }
-        return ts.createArrayLiteral(elements);
+  const transformer = createTransformer(
+    'kAllActiveAbilitiesList',
+    abilities,
+    (ability) => {
+      const element = ts.createObjectLiteral(
+        [
+          ts.createPropertyAssignment(ts.createIdentifier('id'), ts.createStringLiteral(ability.id)),
+          ts.createPropertyAssignment(ts.createIdentifier('humanReadableName'), ts.createStringLiteral(ability.humanReadableName)),
+          ts.createPropertyAssignment(ts.createIdentifier('description'), ts.createStringLiteral(ability.description)),
+          ts.createPropertyAssignment(ts.createIdentifier('target'), ts.createStringLiteral('scan')),
+          ts.createPropertyAssignment(ts.createIdentifier('targetsSignature'), ts.createIdentifier('kNoTarget')),
+          ts.createPropertyAssignment(ts.createIdentifier('cooldownMinutes'), ts.createNumericLiteral(ability.cooldown)),
+          ts.createPropertyAssignment(ts.createIdentifier('karmaCost'), ts.createNumericLiteral(ability.karmaCost)),
+          ts.createPropertyAssignment(ts.createIdentifier('minimalEssence'), ts.createNumericLiteral(0)),
+          ts.createPropertyAssignment(
+            ts.createIdentifier('eventType'),
+            ts.createPropertyAccess(ts.createIdentifier('dummyAbility'), ts.createIdentifier('name')),
+          ),
+        ],
+        true,
+      );
+      addComment(element, 'TODO(aeremin): Add proper implementation');
+      return element;
+    },
+    (ability, propertyName) => {
+      if (propertyName == 'humanReadableName') {
+        return ts.createStringLiteral(ability.humanReadableName);
       }
-
-      if (ts.isObjectLiteralExpression(node)) {
-        const result = ts.visitEachChild(node, abilityVisit, context);
-        if (currentAbility) addComment(result, currentAbility.gmDescription);
-        return result;
+      if (propertyName == 'description') {
+        return ts.createStringLiteral(ability.description);
       }
-
-      if (ts.isPropertyAssignment(node)) {
-        const propertyName = (node.name as ts.Identifier).text;
-        if (propertyName == 'id') {
-          const currentAbilityId = (node.initializer as ts.StringLiteral).text;
-          currentAbility = abilities.find((it) => it.id == currentAbilityId);
-          if (!currentAbility) {
-            console.log(`No data for ${currentAbilityId} in the spreadsheet`);
-          } else {
-            abilities = abilities.filter((it) => it.id != currentAbilityId);
-          }
-        }
-
-        if (currentAbility) {
-          if (propertyName == 'humanReadableName') {
-            return ts.createPropertyAssignment('humanReadableName', ts.createStringLiteral(currentAbility.humanReadableName));
-          }
-          if (propertyName == 'description') {
-            return ts.createPropertyAssignment('description', ts.createStringLiteral(currentAbility.description));
-          }
-          if (propertyName == 'karmaCost') {
-            return ts.createPropertyAssignment('karmaCost', ts.createNumericLiteral(currentAbility.karmaCost));
-          }
-        }
-        if (
-          propertyName == 'target' ||
-          propertyName == 'targetsSignature' ||
-          propertyName == 'prerequisites' ||
-          propertyName == 'minimalEssence' ||
-          propertyName == 'eventType' ||
-          propertyName == 'cooldownMinutes'
-        ) {
-          return node;
-        }
+      if (propertyName == 'karmaCost') {
+        return ts.createNumericLiteral(ability.karmaCost);
       }
-      return ts.visitEachChild(node, abilityVisit, context);
-    };
-
-    const topLevelVisit = (node: ts.Node): ts.Node => {
-      if (ts.isVariableDeclaration(node) && getName(node) == 'kAllActiveAbilitiesList') {
-        const result = ts.visitEachChild(node, abilityVisit, context);
-        return result;
-      } else {
-        return ts.visitEachChild(node, topLevelVisit, context);
-      }
-    };
-
-    return ts.visitNode(rootNode, topLevelVisit);
-  };
+      throw new Error(`Unexpected property name: ${propertyName}`);
+    },
+    ['humanReadableName', 'description', 'karmaCost'],
+  );
 
   writeSourceFile(ts.transform(file, [transformer]).transformed[0], ACTIVE_ABILITIES_FILENAME);
 }
@@ -248,82 +225,45 @@ export function rewriteActiveAbilities(abilities: ActiveAbility[]) {
 export function rewriteSpells(abilities: Spell[]) {
   const file = readSourceFileWithoutComments(SPELLS_FILENAME);
 
-  const transformer = (context: ts.TransformationContext) => (rootNode: ts.SourceFile): ts.SourceFile => {
-    let currentAbility: Spell | undefined = undefined;
-    const abilityVisit = (node: ts.Node): ts.Node => {
-      if (ts.isArrayLiteralExpression(node)) {
-        const existingAbilities = ts.visitEachChild(node, abilityVisit, context);
-        const elements: ts.Expression[] = [...existingAbilities.elements];
-        for (const ability of abilities) {
-          const element = ts.createObjectLiteral(
-            [
-              ts.createPropertyAssignment(ts.createIdentifier('id'), ts.createStringLiteral(ability.id)),
-              ts.createPropertyAssignment(ts.createIdentifier('humanReadableName'), ts.createStringLiteral(ability.humanReadableName)),
-              ts.createPropertyAssignment(ts.createIdentifier('description'), ts.createStringLiteral(ability.description)),
-              ts.createPropertyAssignment(ts.createIdentifier('karmaCost'), ts.createNumericLiteral(ability.karmaCost)),
-              ts.createPropertyAssignment(ts.createIdentifier('sphere'), ts.createStringLiteral(ability.sphere)),
-              ts.createPropertyAssignment(
-                ts.createIdentifier('eventType'),
-                ts.createPropertyAccess(ts.createIdentifier('dummySpell'), ts.createIdentifier('name')),
-              ),
-              ts.createPropertyAssignment(ts.createIdentifier('hasTarget'), ts.createFalse()),
-            ],
-            true,
-          );
-          addComment(element, 'TODO(aeremin): Add proper implementation');
-          addComment(element, ability.gmDescription);
-          elements.push(element);
-        }
-        return ts.createArrayLiteral(elements);
+  const transformer = createTransformer(
+    'kAllSpellsList',
+    abilities,
+    (ability) => {
+      const element = ts.createObjectLiteral(
+        [
+          ts.createPropertyAssignment(ts.createIdentifier('id'), ts.createStringLiteral(ability.id)),
+          ts.createPropertyAssignment(ts.createIdentifier('humanReadableName'), ts.createStringLiteral(ability.humanReadableName)),
+          ts.createPropertyAssignment(ts.createIdentifier('description'), ts.createStringLiteral(ability.description)),
+          ts.createPropertyAssignment(ts.createIdentifier('karmaCost'), ts.createNumericLiteral(ability.karmaCost)),
+          ts.createPropertyAssignment(ts.createIdentifier('sphere'), ts.createStringLiteral(ability.sphere)),
+          ts.createPropertyAssignment(
+            ts.createIdentifier('eventType'),
+            ts.createPropertyAccess(ts.createIdentifier('dummySpell'), ts.createIdentifier('name')),
+          ),
+          ts.createPropertyAssignment(ts.createIdentifier('hasTarget'), ts.createFalse()),
+        ],
+        true,
+      );
+      addComment(element, 'TODO(aeremin): Add proper implementation');
+      return element;
+    },
+    (ability, propertyName) => {
+      if (propertyName == 'humanReadableName') {
+        return ts.createStringLiteral(ability.humanReadableName);
       }
-
-      if (ts.isObjectLiteralExpression(node)) {
-        const result = ts.visitEachChild(node, abilityVisit, context);
-        if (currentAbility) addComment(result, currentAbility.gmDescription);
-        return result;
+      if (propertyName == 'description') {
+        return ts.createStringLiteral(ability.description);
       }
-
-      if (ts.isPropertyAssignment(node)) {
-        const propertyName = (node.name as ts.Identifier).text;
-        if (propertyName == 'id') {
-          const currentAbilityId = (node.initializer as ts.StringLiteral).text;
-          currentAbility = abilities.find((it) => it.id == currentAbilityId);
-          if (!currentAbility) {
-            console.log(`No data for ${currentAbilityId} in the spreadsheet`);
-          } else {
-            abilities = abilities.filter((it) => it.id != currentAbilityId);
-          }
-        }
-
-        if (currentAbility) {
-          if (propertyName == 'humanReadableName') {
-            return ts.createPropertyAssignment('humanReadableName', ts.createStringLiteral(currentAbility.humanReadableName));
-          }
-          if (propertyName == 'description') {
-            return ts.createPropertyAssignment('description', ts.createStringLiteral(currentAbility.description));
-          }
-          if (propertyName == 'karmaCost') {
-            return ts.createPropertyAssignment('karmaCost', ts.createNumericLiteral(currentAbility.karmaCost));
-          }
-        }
-        if (propertyName == 'eventType' || propertyName == 'hasTarget' || propertyName == 'sphere') {
-          return node;
-        }
+      if (propertyName == 'karmaCost') {
+        return ts.createNumericLiteral(ability.karmaCost);
       }
-      return ts.visitEachChild(node, abilityVisit, context);
-    };
-
-    const topLevelVisit = (node: ts.Node): ts.Node => {
-      if (ts.isVariableDeclaration(node) && getName(node) == 'kAllSpellsList') {
-        const result = ts.visitEachChild(node, abilityVisit, context);
-        return result;
-      } else {
-        return ts.visitEachChild(node, topLevelVisit, context);
+      if (propertyName == 'sphere') {
+        return ts.createStringLiteral(ability.sphere);
       }
-    };
-
-    return ts.visitNode(rootNode, topLevelVisit);
-  };
+      throw new Error(`Unexpected property name: ${propertyName}`);
+    },
+    ['humanReadableName', 'description', 'karmaCost', 'sphere'],
+  );
 
   writeSourceFile(ts.transform(file, [transformer]).transformed[0], SPELLS_FILENAME);
 }
