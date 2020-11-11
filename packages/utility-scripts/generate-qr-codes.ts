e3a02d4d465b036e01e860b14cd25d3f1adf475a
Alexey Eremin <a.eremin.msu@gmail.com>
2020-11-11 12:30:17 +0100

    Generate QR codes 2000-2999

--- packages/utility-scripts/generate-qr-codes.ts
+++ packages/utility-scripts/generate-qr-codes.ts
@@ -3,9 +3,11 @@
 
 import * as QRCode from 'qrcode-svg';
 import { encode } from 'alice-qr-lib/qr';
+import { QrCode } from '@sr2020/sr2020-common/models/qr-code.model';
+import * as request from 'request-promise-native';
 
 async function run() {
-  for (let i = 1; i < 1000; ++i) {
+  for (let i = 2000; i < 3000; ++i) {
     const content = encode({ type: 1, kind: 0, validUntil: 1802790262, payload: i.toString() });
     const qrcode = new QRCode(content);
     await new Promise((resolve, reject) => {
@@ -14,6 +16,21 @@ async function run() {
         resolve();
       });
     });
+
+    const qrData: QrCode = {
+      modelId: i.toString(),
+      usesLeft: 0,
+      type: 'empty',
+      name: 'Пустышка',
+      description: 'Не записанный QR-код. На него можно записать что угодно',
+      eventType: '',
+      data: {},
+      timestamp: 0,
+      modifiers: [],
+      timers: [],
+    };
+
+    await request.put('https://models-manager.evarun.ru/qr/model', { json: qrData, resolveWithFullResponse: true }).promise();
   }
 }
 
