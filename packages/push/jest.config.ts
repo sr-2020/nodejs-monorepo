9d42f7c14ccd6c15354dc4668e2d07ca0afa0a5e
Alexey Eremin <a.eremin.msu@gmail.com>
2020-12-07 22:32:17 +0100

    Submit missing Jest configs

---
+++ packages/push/jest.config.ts
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@alice/(.*)$': '<rootDir>/../$1',
+  },
+  testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
+};
