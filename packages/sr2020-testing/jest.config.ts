b75d289701246d1f5f9c24b2062516668c02c6f7
Alexey Eremin <a.eremin.msu@gmail.com>
2020-12-08 21:34:24 +0100

    Increase jest timeout

--- packages/sr2020-testing/jest.config.ts
+++ packages/sr2020-testing/jest.config.ts
@@ -4,5 +4,6 @@ module.exports = {
   moduleNameMapper: {
     '^@alice/(.*)$': '<rootDir>/../$1',
   },
+  setupFilesAfterEnv: ['./set-jest-timeout.ts'],
   testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
 };
