6d2c92d4f95820e8fdfe0c64010fb9376973ec41
Alexey Eremin <a.eremin.msu@gmail.com>
2020-12-08 23:23:54 +0100

    Migrate alice-models-manager to nx

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  projects: ['<rootDir>/packages/interface', '<rootDir>/packages/push', '<rootDir>/packages/alice-model-engine'],
+  projects: [
+    '<rootDir>/packages/interface',
+    '<rootDir>/packages/push',
+    '<rootDir>/packages/alice-model-engine',
+    '<rootDir>/packages/alice-models-manager',
+  ],
 };
