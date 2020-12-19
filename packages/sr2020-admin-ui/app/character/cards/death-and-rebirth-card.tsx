b9a86725888f36b1c4e209d79abea0fae21c5508
Alexey Eremin <a.eremin.msu@gmail.com>
2020-12-19 18:09:33 +0100

    Add button to move character to "clinically dead" state

--- packages/sr2020-admin-ui/app/character/cards/death-and-rebirth-card.tsx
+++ packages/sr2020-admin-ui/app/character/cards/death-and-rebirth-card.tsx
@@ -7,6 +7,10 @@ export class DeathAndRebirthCard extends React.Component<{ sendEvent: SendEvent 
     this.props.sendEvent('wound', {});
   }
 
+  clinicalDeath() {
+    this.props.sendEvent('clinicalDeath0MaxHp', {});
+  }
+
   revive() {
     this.props.sendEvent('debugReviveAbsolute', {});
   }
@@ -16,8 +20,11 @@ export class DeathAndRebirthCard extends React.Component<{ sendEvent: SendEvent 
       <Card body={false}>
         <Card.Header>Ранения и смерть</Card.Header>
         <Card.Body>
-          <Button variant="danger" onClick={() => this.wound()}>
+          <Button variant="warning" onClick={() => this.wound()}>
             Упасть в тяжран
+          </Button>
+          <Button variant="danger" onClick={() => this.clinicalDeath()}>
+            Упасть в КС
           </Button>
           <Button variant="success" onClick={() => this.revive()}>
             Воскресить
