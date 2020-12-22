56c2ca4bf8f3c22b9f68d6ac5ab9e38688d671e3
Alexey Eremin <a.eremin.msu@gmail.com>
2020-12-22 17:08:31 +0100

    Add character scripts allowing to apply migrations

---
+++ packages/sr2020-model-engine/scripts/character/development.ts
@@ -0,0 +1,21 @@
+import { EventModelApi, Modifier, UserVisibleError } from '@alice/alice-common/models/alice-model-engine';
+import { Sr2020Character } from '@alice/sr2020-common/models/sr2020-character.model';
+
+// Migrates to updated character model (e.g. sets some optional fields to the default values).
+export function developmentMigrate(api: EventModelApi<Sr2020Character>, data: never) {
+  for (const modifier of api.model.modifiers) {
+    if (modifier.priority == undefined) {
+      modifier.priority = Modifier.kDefaultPriority;
+    }
+  }
+}
+
+// Validates that character model is ready to be updated to a new "schema"
+// (e.g. that some optional fields can be made required).
+export function developmentValidate(api: EventModelApi<Sr2020Character>, data: never) {
+  for (const modifier of api.model.modifiers) {
+    if (modifier.priority == undefined) {
+      throw new UserVisibleError(`Modifier ${modifier.mID} of character ${api.model.modelId} has no priority`);
+    }
+  }
+}
