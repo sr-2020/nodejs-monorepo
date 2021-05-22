5c421171d9934be08d99bcc668422650c954d3ac
Alexey Eremin <a.eremin.msu@gmail.com>
2021-05-22 23:42:57 +0200

    Fix sprite type check

--- packages/sr2020-model-engine/scripts/character/active_abilities.ts
+++ packages/sr2020-model-engine/scripts/character/active_abilities.ts
@@ -352,7 +352,7 @@ export function activateSoft(api: EventModelApi<Sr2020Character>, data: ActiveAb
 
 export function useSpriteAbility(api: EventModelApi<Sr2020Character>, data: ActiveAbilityData) {
   const sprite = typedQrData<SpriteQrData>(api.aquired(QrCode, data.qrCodeId!));
-  const canUse = api.workModel.passiveAbilities.some((ability) => `$sprite-{ability.id}` == sprite.id);
+  const canUse = api.workModel.passiveAbilities.some((ability) => `sprite-${ability.id}` == sprite.id);
   if (!canUse) {
     throw new UserVisibleError('Вы не умеете работать со спрайтами этого типа!');
   }
