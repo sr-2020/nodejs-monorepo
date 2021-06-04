b56763638ecfe3b59a2f837c4e7c2c6555139156
Murphy812 <murmail@gmail.com>
2021-06-04 08:22:26 +0300

    new reagents: Might1-2, Might1-5 per race and sphere alone

--- packages/sr2020-model-engine/scripts/qr/reagents_library.ts
+++ packages/sr2020-model-engine/scripts/qr/reagents_library.ts
@@ -138,4 +138,134 @@ export const kAllReagents: Reagent[] = [
     name: 'Голландский штурвал',
     content: { aquarius: 5 },
   },
+  {
+    id: 'fish-n-fish',
+    name: 'Фиш-н-фиш',
+    content: { pisces: 3 },
+  },
+  {
+    id: 'oyboy',
+    name: 'Ойбой',
+    content: { sagittarius: 3 },
+  },
+  {
+    id: 'lion2lion',
+    name: 'Львиный лев',
+    content: { leo: 3 },
+  },
+  {
+    id: 'overweight',
+    name: 'Перевес',
+    content: { libra: 3 },
+  },
+  {
+    id: 'teardrop',
+    name: 'Капли дождя',
+    content: { aquarius: 3 },
+  },
+  {
+    id: 'stingday',
+    name: 'Жало дней',
+    content: { scorpio: 3 },
+  },
+  {
+    id: 'elliell',
+    name: 'Эльлилиэль',
+    content: { virgo: 2 },
+  },
+  {
+    id: 'bulls-by-bulls',
+    name: 'Бык-о-бык',
+    content: { taurus: 2 },
+  },
+  {
+    id: 'bridge-tour',
+    name: 'Встреча на мосту',
+    content: { aries: 2 },
+  },
+  {
+    id: 'cc-axe',
+    name: 'Двурачный топор',
+    content: { cancer: 2 },
+  },
+  {
+    id: 'ivan-da-maria',
+    name: 'Иван-да-Марья',
+    content: { gemini: 2 },
+  },
+  {
+    id: 'crimson-ing',
+    name: 'Кровавый подбой',
+    content: { capricorn: 2 },
+  },
+  {
+    id: 'aquarium',
+    name: 'Аквариум',
+    content: { pisces: 8 },
+  },
+  {
+    id: 'young-and-bright',
+    name: 'Молодой да резвый',
+    content: { sagittarius: 8 },
+  },
+  {
+    id: 'lay-pride',
+    name: 'Прайд',
+    content: { leo: 8 },
+  },
+  {
+    id: 'apothecarius',
+    name: 'Мерная палата',
+    content: { libra: 8 },
+  },
+  {
+    id: 'super-mario',
+    name: 'Супер Марио',
+    content: { aquarius: 8 },
+  },
+  {
+    id: 'scorping',
+    name: 'Скорпинг',
+    content: { scorpio: 8 },
+  },
+  {
+    id: 'elvanovo',
+    name: 'Эльфанова',
+    content: { virgo: 5 },
+  },
+  {
+    id: 'bychat',
+    name: 'Бычат',
+    content: { taurus: 5 },
+  },
+  {
+    id: 'barash-shur',
+    name: 'Бараш Шур',
+    content: { aries: 5 },
+  },
+  {
+    id: 'fast-enough',
+    name: 'Достаточная скорость',
+    content: { cancer: 5 },
+  },
+  {
+    id: 'what-is-the-way',
+    name: 'Армия клонов',
+    content: { gemini: 5 },
+  },
+  {
+    id: 'fantabeasts',
+    name: 'Фантастические твари',
+    content: { capricorn: 5 },
+  },
+  {
+    id: 'mama-ama-mag',
+    name: 'Мама мыла раму',
+    content: { virgo: 3, taurus: 3, aries: 3, cancer: 3, gemini: 3, capricorn: 3, pisces: 3, sagittarius: 3, leo: 3, libra: 3, aquarius: 3, scorpio: 3 },
+  },
+  {
+    id: 'react-at-all',
+    name: 'Реагируй полностью',
+    content: { virgo: 20, taurus: 20, aries: 20, cancer: 20, gemini: 20, capricorn: 20, pisces: 20, sagittarius: 20, leo: 20, libra: 20, aquarius: 20, scorpio: 20 },
+  },
 ];
