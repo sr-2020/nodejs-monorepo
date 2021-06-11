cb5ba1e7901b56c145ad310454fb975076c1a6b2
Alexey Eremin <a.eremin.msu@gmail.com>
2021-06-11 09:36:52 +0200

    Use authentificated models-manager endpoint in admin-ui

--- packages/sr2020-admin-ui/app/api/models-manager.ts
+++ packages/sr2020-admin-ui/app/api/models-manager.ts
@@ -1,39 +1,53 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { Sr2020Character, Sr2020CharacterProcessResponse } from '@alice/sr2020-common/models/sr2020-character.model';
 import { EventRequest } from '@alice/alice-common/models/alice-model-engine';
 import { QrCode, QrCodeProcessResponse } from '@alice/sr2020-common/models/qr-code.model';
 
-const MODELS_MANAGER_URL = 'https://models-manager.evarun.ru/';
+const MODELS_MANAGER_URL = 'https://gateway.evarun.ru/api/v1/models-manager/';
 const MODELS_MANAGER_CHARACTER_URL = MODELS_MANAGER_URL + 'character/model/';
 const MODELS_MANAGER_QR_URL = MODELS_MANAGER_URL + 'qr/model/';
 
+function config(): AxiosRequestConfig {
+  const token = document.cookie.match(/Authorization=([^;]*)/)[1];
+  return {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+}
+
 export async function getCharacter(id: string): Promise<Sr2020Character> {
-  const response = await axios.get<Sr2020CharacterProcessResponse>(MODELS_MANAGER_CHARACTER_URL + id);
+  console.log(document.cookie.match(/Authorization=[^;]*/)[0]);
+  const response = await axios.get<Sr2020CharacterProcessResponse>(MODELS_MANAGER_CHARACTER_URL + id, config());
   return response.data.workModel;
 }
 
 export async function sendCharacterEvent(id: string, event: EventRequest): Promise<Sr2020Character> {
-  const response = await axios.post<Sr2020CharacterProcessResponse>(MODELS_MANAGER_CHARACTER_URL + id, event);
+  const response = await axios.post<Sr2020CharacterProcessResponse>(MODELS_MANAGER_CHARACTER_URL + id, event, config());
   return response.data.workModel;
 }
 
 export async function broadcastCharacterEvent(event: EventRequest): Promise<void> {
-  await axios.post<Sr2020CharacterProcessResponse>(MODELS_MANAGER_CHARACTER_URL.replace('/model/', '/broadcast/'), event);
+  await axios.post<Sr2020CharacterProcessResponse>(MODELS_MANAGER_CHARACTER_URL.replace('/model/', '/broadcast/'), event, config());
 }
 
 export async function setDefaultCharacter(id: string, name: string): Promise<Sr2020Character> {
-  const response = await axios.put<Sr2020CharacterProcessResponse>(MODELS_MANAGER_CHARACTER_URL.replace('/model/', '/default/') + id, {
-    name,
-  });
+  const response = await axios.put<Sr2020CharacterProcessResponse>(
+    MODELS_MANAGER_CHARACTER_URL.replace('/model/', '/default/') + id,
+    {
+      name,
+    },
+    config(),
+  );
   return response.data.workModel;
 }
 
 export async function getQr(id: string): Promise<QrCode> {
-  const response = await axios.get<QrCodeProcessResponse>(MODELS_MANAGER_QR_URL + id);
+  const response = await axios.get<QrCodeProcessResponse>(MODELS_MANAGER_QR_URL + id, config());
   return response.data.workModel;
 }
 
 export async function sendQrEvent(id: string, event: EventRequest): Promise<QrCode> {
-  const response = await axios.post<QrCodeProcessResponse>(MODELS_MANAGER_QR_URL + id, event);
+  const response = await axios.post<QrCodeProcessResponse>(MODELS_MANAGER_QR_URL + id, event, config());
   return response.data.workModel;
 }
