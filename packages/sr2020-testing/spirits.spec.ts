02c3d43f580cf180a142f284009e0110a59cba87
Alexander Chuprin <murmail@gmail.com>
2021-07-15 21:32:57 +0300

    Update spirits.spec.ts

--- packages/sr2020-testing/spirits.spec.ts
+++ packages/sr2020-testing/spirits.spec.ts
@@ -13,43 +13,6 @@ describe('Spirits-related abilities', () => {
     await fixture.destroy();
   });
 
-  // it('Entering and leaving spirit', async () => {
-  //   // Mage set up
-  //   await fixture.saveCharacter({ name: 'Vasya', maxHp: 2, magic: 5 });
-
-  //   const spirit: Spirit = {
-  //     name: 'Petya',
-  //     hp: 6,
-  //     abilityIds: ['magic-3', 'dispirit'],
-  //   };
-  //   // Enter spirit
-  //   await fixture.sendCharacterEvent({ eventType: 'enterSpirit', data: spirit });
-
-  //   {
-  //     // Mage is in spirit and has proper abilities and hp
-  //     const { workModel } = await fixture.getCharacter();
-  //     expect(workModel.name).toBe('Petya');
-  //     expect(workModel.maxHp).toBe(6);
-  //     expect(workModel.magic).toBe(6);
-  //     expect(workModel.activeAbilities).toContainEqual(expect.objectContaining({ id: 'dispirit' }));
-  //     expect(workModel.currentBody).toBe('ectoplasm');
-  //   }
-
-  //   // Leave spirit
-  //   await fixture.useAbility({ id: 'dispirit' });
-
-  //   {
-  //     // Mage is not in the spirit
-  //     const { workModel } = await fixture.getCharacter();
-  //     expect(workModel.name).toBe('Vasya');
-  //     expect(workModel.maxHp).toBe(2);
-  //     expect(workModel.magic).toBe(5);
-  //     expect(workModel.passiveAbilities).not.toContainEqual(expect.objectContaining({ id: 'magic-3' }));
-  //     expect(workModel.activeAbilities).toHaveLength(0); // Enter spirit
-  //     expect(workModel.currentBody).toBe('physical');
-  //   }
-  // });
-
   it('Put spirit in jar and free it', async () => {
     await fixture.saveQrCode();
     {
