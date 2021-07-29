582bc2f13d8d9e0cd15601e4dd517232a4bac303
Alexey Eremin <a.eremin.msu@gmail.com>
2021-07-29 16:27:46 +0200

    Fix qr id format

--- packages/sr2020-admin-ui/app/qr/cards/copy-another-card.tsx
+++ packages/sr2020-admin-ui/app/qr/cards/copy-another-card.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Button, Card, FormControl, InputGroup } from 'react-bootstrap';
 import { SendEvent } from '@alice/sr2020-admin-ui/app/api/event-sender';
 
-export class CopyAnotherCard extends React.Component<{ sendEvent: SendEvent }, { qrCodeId: number }> {
-  state = { qrCodeId: 1 };
+export class CopyAnotherCard extends React.Component<{ sendEvent: SendEvent }, { qrCodeId: string }> {
+  state = { qrCodeId: '1' };
 
   write() {
     return this.props.sendEvent('pasteAnother', this.state);
@@ -18,7 +18,7 @@ export class CopyAnotherCard extends React.Component<{ sendEvent: SendEvent }, {
             <InputGroup.Prepend>
               <InputGroup.Text>Откуда копируем (будет очищен!!!)</InputGroup.Text>
             </InputGroup.Prepend>
-            <FormControl type="number" value={this.state.qrCodeId} onChange={(e) => this.setState({ qrCodeId: Number(e.target.value) })} />
+            <FormControl type="number" value={this.state.qrCodeId} onChange={(e) => this.setState({ qrCodeId: e.target.value })} />
             <InputGroup.Append>
               <Button variant="success" onClick={() => this.write()}>
                 Вырезать и вставить
